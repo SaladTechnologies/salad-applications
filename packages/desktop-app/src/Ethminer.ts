88cb13bcded1b669e30a0d844ea3ff04ea2dd47f
Daniel Sarfati <danielrsarfati@gmail.com>
2019-03-26 19:55:14 -0600

    Added ethminer from 0.1.7C

---
+++ packages/desktop-app/src/Ethminer.ts
@@ -0,0 +1,86 @@
+import { spawn, ChildProcess, exec } from 'child_process'
+import { MachineInfo } from './models/MachineInfo'
+
+export class Ethminer {
+  childProcess?: ChildProcess
+  isRunning = false
+  processName: string = ''
+
+  start = (machineInfo: MachineInfo) => {
+    if (this.childProcess || this.isRunning) {
+      console.log('Ethminer already running and cannot be started again.')
+      return
+    }
+
+    this.isRunning = true
+
+    let cuda = machineInfo.gpus.some(x => x.vendor.toLocaleLowerCase().includes('nvidia'))
+
+    console.log('cuda: ' + cuda)
+
+    let id = 'daniel'
+    let platform = cuda ? '-U' : '-G'
+    this.processName = cuda ? 'ethminer_cuda.exe' : 'ethminer.exe'
+    let cmd = `cd dist && cd ethminer && ${
+      this.processName
+    } --farm-recheck 1000 ${platform} -P stratum1+tcp://0x6fF85749ffac2d3A36efA2BC916305433fA93731@eth-us-west1.nanopool.org:9999/${id}/notinuse@salad.io`
+    let ls = spawn(cmd, {
+      shell: true,
+      windowsHide: true,
+    })
+
+    this.childProcess = ls
+
+    if (ls.stdout) {
+      ls.stdout.on('data', data => {
+        console.log('stdout: ' + data)
+      })
+    }
+
+    if (ls.stderr) {
+      ls.stderr.on('data', data => {
+        console.log('stdout: ' + data)
+      })
+    }
+
+    ls.on('close', code => {
+      console.log('closed with exit code: ' + code)
+    })
+
+    ls.on('error', err => {
+      console.log('error: ' + err)
+    })
+
+    ls.on('exit', msg => {
+      console.log('exit: ' + msg)
+    })
+  }
+
+  stop = () => {
+    if (this.childProcess && !this.childProcess.killed) {
+      exec(`taskkill /im ${this.processName} /t /f`, () => {
+        console.log('miner is stopped')
+        this.childProcess = undefined
+        this.isRunning = false
+      })
+    } else {
+      console.log('No process to kill.')
+    }
+  }
+}
+//     if (isAmdGpu) {
+//       ls = spawn(
+//         `cd mining && cd ethMiner_v1.17 && setx GPU_FORCE_64BIT_PTR 0 && setx GPU_MAX_HEAP_SIZE 100 && setx GPU_USE_SYNC_OBJECTS 1 && setx GPU_SINGLE_ALLOC_PERCENT 100 && ethminer.exe --farm-recheck 1000 -G -P stratum1+tcp://0x6fF85749ffac2d3A36efA2BC916305433fA93731@eth-us-west1.nanopool.org:9999/${macAddress}/notinuse@salad.io`,
+//         {
+//           shell: true,
+//         },
+//       )
+//     } else {
+//       // for NVIDIA
+//       ls = spawn(
+//         `cd mining && cd ethMiner_v1.17 && setx GPU_FORCE_64BIT_PTR 0 && setx GPU_MAX_HEAP_SIZE 100 && setx GPU_USE_SYNC_OBJECTS 1 && setx GPU_SINGLE_ALLOC_PERCENT 100 && ethminer_cuda.exe --farm-recheck 1000 -U -P stratum1+tcp://0x6fF85749ffac2d3A36efA2BC916305433fA93731@eth-us-west1.nanopool.org:9999/${macAddress}/notinuse@salad.io`,
+//         {
+//           shell: true,
+//         },
+//       )
+//     }
--- packages/desktop-app/src/main.ts
+++ packages/desktop-app/src/main.ts
@@ -4,10 +4,33 @@ import * as path from 'path'
 import * as si from 'systeminformation'
 import { SaladBridge } from './SaladBridge'
 import { Config } from './config'
+import { Ethminer } from './Ethminer'
+import { MachineInfo } from './models/MachineInfo'
 
 let mainWindow: Electron.BrowserWindow
+let machineInfo: MachineInfo
+let ethminer = new Ethminer()
 
-function onReady() {
+const getMachineInfo = () =>
+  new Promise<MachineInfo>((resolve, reject) => {
+    si.graphics()
+      .then(graphics => {
+        si.system().then(net => {
+          console.log(net)
+          si.osInfo().then(os => {
+            //TODO: Figure out what mac/UUID we are going to use
+            machineInfo = {
+              os: os,
+              gpus: graphics.controllers,
+            }
+            resolve(machineInfo)
+          })
+        })
+      })
+      .catch(() => reject())
+  })
+
+const onReady = () => {
   mainWindow = new BrowserWindow({
     title: 'Daniel',
     minWidth: 1216,
@@ -27,6 +50,8 @@ function onReady() {
   mainWindow.loadURL(Config.appUrl)
   mainWindow.on('close', () => app.quit())
   mainWindow.once('ready-to-show', () => {
+    //Pre-fetch the machine info
+    getMachineInfo()
     mainWindow.show()
   })
 
@@ -36,19 +61,22 @@ function onReady() {
 
   //Listen for machine info requests
   bridge.on('get-machine-info', () => {
-    si.graphics().then(graphics => {
-      si.osInfo().then(os => {
-        bridge.send('set-machine-info', {
-          os: os,
-          gpus: graphics.controllers,
-        })
-      })
+    //Return the cached machine info
+    if (machineInfo) {
+      bridge.send('set-machine-info', machineInfo)
+    }
+
+    //Fetch the machine info
+    getMachineInfo().then(info => {
+      machineInfo = info
+      bridge.send('set-machine-info', machineInfo)
     })
   })
 
   bridge.on('minimize-window', () => {
     mainWindow.minimize()
   })
+
   bridge.on('maximize-window', () => {
     if (!mainWindow.isMaximized()) {
       mainWindow.maximize()
@@ -56,11 +84,52 @@ function onReady() {
       mainWindow.unmaximize()
     }
   })
+
   bridge.on('close-window', () => {
     mainWindow.close()
+  })
+
+  bridge.on('start', () => {
+    console.log('Starting salad')
+
+    if (machineInfo) {
+      ethminer.start(machineInfo)
+    } else {
+      getMachineInfo().then(info => {
+        ethminer.start(info)
+      })
+    }
+  })
+
+  bridge.on('stop', () => {
+    console.log('Stopping salad')
+    ethminer.stop()
   })
 }
 
 app.on('ready', () => onReady())
-app.on('window-all-closed', () => app.quit())
+app.on('will-quit', () => {
+  console.log('will quit')
+  ethminer.stop()
+})
+
+process.on('exit', () => {
+  console.log('Process exit')
+  ethminer.stop()
+})
+
+app.on('window-all-closed', () => {
+  console.log('window-all-closed')
+  if (process.platform != 'darwin') {
+    app.quit()
+  }
+})
+
+const cleanExit = () => {
+  console.log('clean-exit')
+  process.exit()
+}
+process.on('SIGINT', cleanExit) // catch ctrl-c
+process.on('SIGTERM', cleanExit) // catch kill
+
 console.log(`Electron Version ${app.getVersion()}`)
