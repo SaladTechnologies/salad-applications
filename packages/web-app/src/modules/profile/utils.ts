5569712c5ed65265fa3bb47744ea80b73680ff5d
Daniel Sarfati <danielrsarfati@gmail.com>
2019-04-11 14:02:32 -0600

    Allow the user to skip the referral page

--- packages/web-app/src/modules/profile/utils.ts
+++ packages/web-app/src/modules/profile/utils.ts
@@ -1,19 +1,21 @@
 import { ProfileResource } from './models/ProfileResource'
 import { Profile, ReferredStatus } from './models/Profile'
 
-export const profileFromResource = (r: ProfileResource): Profile => ({
+export const profileFromResource = (r: ProfileResource, skippedReferral: boolean | undefined): Profile => ({
   id: String(r.userId),
   username: String(r.profileData.nickname),
   email: String(r.profileData.email),
   termsOfService: String(r.termsOfService),
   whatsNewVersion: String(r.whatsNewVersion),
-  referred: getReferredStatus(r.isReferred),
+  referred: getReferredStatus(r.isReferred, skippedReferral),
   trackUsageVersion: String(r.trackUsageVersion),
   tutorialComplete: r.tutorialComplete === 1,
 })
 
-const getReferredStatus = (isReferred: string): ReferredStatus => {
+const getReferredStatus = (isReferred: string, skippedReferral: boolean | undefined): ReferredStatus => {
   let statusCode = Number(isReferred)
+
+  if (skippedReferral) return ReferredStatus.NotReferred
 
   //If the code is not a number, it is a valid code
   if (statusCode === NaN) {
