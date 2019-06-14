b70dca2a0c769b8d064868e88f4c21bcbc97374a
OneJChristensen <joshua@thedigitaldesign.com>
2019-06-14 13:43:30 -0600

    Doing some building out of the UI componentes

---
+++ packages/web-app/src/ui/percentages/Percentage.styles.tsx
@@ -0,0 +1,11 @@
+// Theme
+import { SaladTheme } from '../../SaladTheme'
+
+export const styles = (theme: SaladTheme) => ({
+  percentage: {
+    color: theme.orange,
+    fontFamily: theme.fontGroteskLight05,
+    fontSize: theme.xxLarge,
+    lineHeight: theme.xxLarge,
+  }
+})
