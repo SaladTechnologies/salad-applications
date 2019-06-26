e442f6f90563a6ed69f62badefb5bb9bffaafd94
Daniel Sarfati <danielrsarfati@gmail.com>
2019-06-26 10:18:56 -0600

    Updated notification banner copy for referral bonus competition DEV-150

--- packages/web-app/src/modules/home-views/NotificationBannerContainer.tsx
+++ packages/web-app/src/modules/home-views/NotificationBannerContainer.tsx
@@ -4,9 +4,9 @@ import { NotificationBanner } from './components/NotificationBanner'
 // These values wont change when time updates, they will only be used when the component loads. In the future we could update these dynamically.
 const mapStoreToProps = () => ({
   now: new Date(),
-  startDate: new Date('May 16, 2019 12:00:00 GMT-06:00'),
-  endDate: new Date('May 22, 2019 12:00:00 GMT-06:00'),
-  text: '2X Earnings Week: May 16 - May 22',
+  startDate: new Date('June 26, 2019 12:00:00 GMT-06:00'),
+  endDate: new Date('June 30, 2019 12:00:00 GMT-06:00'),
+  text: '$100 Prize Referral Contest! See Discord for Deets.',
 })
 
 export const NotificationBannerContainer = connect(
