6bcbe2c1fffa285a98b528a7cef3d1361fa28e2d
Damon Sorensen <damonsorensen@gmail.com>
2019-07-02 14:11:07 -0600

    added canny

--- packages/web-app/src/modules/home-views/BottomBarContainer.tsx
+++ packages/web-app/src/modules/home-views/BottomBarContainer.tsx
@@ -1,13 +1,41 @@
 import { BottomBar } from './components/BottomBar'
 import { connect } from '../../connect'
 import { Config } from '../../config'
+import { RootStore } from '../../Store';
+
 
 const handleDiscord = () => {
   openLink(Config.discordUrl)
 }
 
-const handleSupport = () => {
-  openLink(Config.supportUrl)
+const handleSupport = (store: RootStore) => {
+
+  if( store.profile.currentProfile !== undefined){
+  //@ts-ignore
+  Canny('identify', {
+    appID: '5d1b944c723f2266bea8f5b7',
+    user: {
+      // Replace these values with the current user's data
+      email: store.profile.currentProfile.email,
+      name: store.profile.currentProfile.username,
+      id: store.profile.currentProfile.id, 
+
+      // // These fields are optional, but recommended:
+      avatarURL: 'https://s3.us-east-2.amazonaws.com/salad-ui-assets/cool-carrot.png'
+      // created: new Date(user.created).toISOString(),
+    }
+  }, 
+  ()=>{
+    console.log('canny recieved');
+    openLink('https://feedback.salad.io')
+  }
+  )
+} else{
+  openLink('https://feedback.salad.io')
+
+}
+
+  
 }
 
 const handleVersion = () => {
@@ -18,9 +46,9 @@ const openLink = (url: string) => {
   window.open(url, '_blank')
 }
 
-const mapStoreToProps = () => ({
+const mapStoreToProps = (store: RootStore) => ({
   onDiscordClick: handleDiscord,
-  onSupportClick: handleSupport,
+  onSupportClick: () => handleSupport(store),
   onVersionClick: handleVersion,
   version: Config.appVersion,
 })
