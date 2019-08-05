86939921bb0a139f7b354d03ecc525b3496c4a4b
mkjenkins <mkjxocai@gmail.com>
2019-08-05 13:20:31 -0600

    Added 3 new veggies and normalized lowbies
    Added 3 new veggies and normalized lowbies

--- packages/web-app/src/modules/xp/models/defaultLevels.ts
+++ packages/web-app/src/modules/xp/models/defaultLevels.ts
@@ -1,6 +1,9 @@
 import { Level } from './Level'
 
 export const defaultLevels = [
+  new Level('blue-cheese', 'Blue Cheese', 150000, 250000, '#f4efe4'),
+  new Level('broccoli', 'Broccoli', 100000, 150000, '#53a626'),
+  new Level('olives', 'Olives', 75000, 100000, '#1f4f22'),
   new Level('red-onion', 'Red Onion', 50000, 75000, '#7d0328'),
   new Level('avocado', 'Avocado', 30000, 50000, '#b2d530'),
   new Level('red-pepper', 'Red Pepper', 20000, 30000, '#d1031a'),
