88d005ab1873dc6c20fc5cb09c3c1af7c1de025d
Daniel Sarfati <danielrsarfati@gmail.com>
2019-08-09 12:12:54 -0600

    Added reward selection

--- packages/web-app/src/modules/data-refresh/RefreshService.ts
+++ packages/web-app/src/modules/data-refresh/RefreshService.ts
@@ -18,25 +18,26 @@ export class RefreshService {
     this.rewardsTimer = setInterval(() => {
       this.store.rewards.refreshRewards()
     }, Config.rewardsRefreshRate)
-    
+
     //Do the initial data pull
     this.refreshData()
     this.store.rewards.refreshRewards()
   }
 
-  refreshData =() =>{
+  refreshData = () => {
     if (!this.store.auth.isAuthenticated()) {
       return
     }
     try {
       /*
         TODO:
-          - this.rewards.loadDataRefresh
           - this.referral.loadDataRefresh
           - this.machine.loadDataRefresh
-      */ 
+      */
+
       this.store.xp.refreshXp()
       this.store.balance.loadDataRefresh()
+      this.store.rewards.loadSelectedReward()
     } catch (error) {
       console.error(error)
     }
@@ -48,4 +49,3 @@ export class RefreshService {
     if (this.rewardsTimer) clearInterval(this.rewardsTimer)
   }
 }
-
