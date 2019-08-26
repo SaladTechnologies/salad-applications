2d357d55889cf77d6b39fab46275bb63836c06cf
Daniel Sarfati <danielrsarfati@gmail.com>
2019-08-26 10:57:04 -0600

    Moved lifetime balance into start button (#127)

--- packages/web-app/src/modules/balance/models/Earnings.ts
+++
@@ -1,4 +0,0 @@
-export interface Earnings {
-  lastDayEarnings: number
-  lastHourEarnings: number
-}
--- packages/web-app/src/modules/machine-views/StartButtonContainer.ts
+++ packages/web-app/src/modules/machine-views/StartButtonContainer.ts
@@ -4,10 +4,10 @@ import { StartButton } from './components/StartButton'
 
 const mapStoreToProps = (store: RootStore) => ({
   balance: store.balance.currentBalance,
-  earnings: store.balance.earnings,
+  lifetimeBalance: store.balance.lifetimeBalance,
   onClick: store.native.toggleRunning,
   isRunning: store.native.isRunning,
-  startEnabled: true, // TODO: 0.2.1 - store.native.isNative && store.native.isCompatible,
+  startEnabled: store.native.isNative && store.native.isCompatible,
 })
 
 export const StartButtonContainer = connect(
