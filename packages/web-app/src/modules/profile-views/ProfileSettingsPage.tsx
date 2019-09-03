2c0cfca42369fdc06a265a72b0aab3f395ab2193
Damon Sorensen <damonsorensen@gmail.com>
2019-09-03 09:00:48 -0600

    Edit Username DEV-360 (#142)
    * Edit Username
    
    * RETARD
    
    * Error messages fixed

---
+++ packages/web-app/src/modules/profile-views/ProfileSettingsPage.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { SaladTheme } from '../../SaladTheme'
+import withStyles, { WithStyles } from 'react-jss'
+import { EditUsernameContainer } from './EditUsernameContainer';
+
+const styles = (theme: SaladTheme) => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '2rem',
+  },
+  column: {
+    flexGrow: 1,
+    flexBasis: 0,
+    marginRight: '2rem',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+})
+
+interface Props extends WithStyles<typeof styles> {
+  
+}
+
+class _ProfileSettingsPage extends Component<Props> {
+  render() {
+    // const { classes } = this.props
+
+    return (
+      <div >
+        <EditUsernameContainer/>
+      </div>
+    )
+  }
+}
+
+export const ProfileSettingsPage = withStyles(styles)(_ProfileSettingsPage)
