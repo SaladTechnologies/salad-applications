324ffb780cdf4ee979ae2beef16582285dc9a4a1
Damon <damonsorensen@gmail.com>
2019-09-05 10:05:42 -0500

    made log plural

--- packages/web-app/src/modules/settings-views/components/Settings.tsx
+++ packages/web-app/src/modules/settings-views/components/Settings.tsx
@@ -93,7 +93,7 @@ class _Settings extends Component<Props> {
           <div>{menuItems && <LinkListUnstyled list={menuItems} onListItemClick={this.handleListItemClick} />}</div>
           <div className={classes.buttonContainer}>
             <Button onClick={this.handleBugClicked}>Submit Bug</Button>
-            <Button onClick={this.handleLogClicked}>Send Log</Button>
+            <Button onClick={this.handleLogClicked}>Send Logs</Button>
           </div>
           <div className={classes.versionContainer}>
             <MenuTitle>Version: {appVersion ? appVersion : '-'}</MenuTitle>
