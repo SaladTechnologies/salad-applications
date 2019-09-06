dce029310cbbc43b4684a4d85f5b766f4a7a5c09
Daniel Sarfati <danielrsarfati@gmail.com>
2019-09-06 16:39:17 -0600

    Updated referral models (#164)
    * Fixed machine info model
    
    * Updated referral definition models

--- packages/web-app/src/modules/referral/models/ReferralDefinition.ts
+++ packages/web-app/src/modules/referral/models/ReferralDefinition.ts
@@ -1,14 +1,8 @@
-export class ReferralDefinition {
-  balanceThreshold: number = 0
-  bonusRate: number = 0
-  referrerBonus: number = 0
-
-  /** The maximum bonus the referrer can earn (USD) */
-  get maximumReferrerBonus(): number {
-    return this.balanceThreshold * this.referrerBonus
-  }
-
-  public constructor(init?: Partial<ReferralDefinition>) {
-    Object.assign(this, init)
-  }
+export interface ReferralDefinition {
+  balanceThreshold: number
+  bonusRate: number
+  referrerBonus: number
 }
+
+export const maximumReferrerBonus = (referralDefinition: ReferralDefinition): number =>
+  referralDefinition.balanceThreshold * referralDefinition.referrerBonus
