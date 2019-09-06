33c6012339292b376ac458db8cdb621069fb1c4b
Damon Sorensen <damonsorensen@gmail.com>
2019-09-05 19:14:51 -0600

    Updated Alpha Badge DEV-418 (#149)
    * Updated Alpha Badge
    
    * implemented requested changes

--- packages/web-app/src/modules/profile-views/components/ProfileSummaryTitle.tsx
+++ packages/web-app/src/modules/profile-views/components/ProfileSummaryTitle.tsx
@@ -6,7 +6,7 @@ import classnames from 'classnames'
 
 const styles = (theme: SaladTheme) => ({
   container: {
-    padding: '1rem',
+    padding: '1rem 1rem 0',
   },
   username: {
     fontFamily: 'sharpGroteskLight25',
