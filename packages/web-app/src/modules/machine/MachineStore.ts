515db68c276f38cd7663b08cea1c661ad63d6abc
Joshua Christensen <joshua@salad.io>
2019-09-09 14:23:07 -0600

    Moving logic around
    - Moved earning rate formatting out of the store and into the controller

--- packages/web-app/src/modules/machine/MachineStore.ts
+++ packages/web-app/src/modules/machine/MachineStore.ts
@@ -1,6 +1,5 @@
 import { action, observable, computed } from 'mobx'
 import { Machine } from './models/Machine'
-import { RootStore } from '../../Store'
 
 export class MachineStore {
   @observable
@@ -10,25 +9,6 @@ export class MachineStore {
   get currentEarningRate(): number {
     return this.currentMachine ? this.currentMachine.earningRate : 0
   }
-
-  @computed
-  get formattedCurrentEarningRate(): string {
-    let earningRate = 'Loading'
-
-    if (this.store.native.machineInfo) {
-      if (this.currentMachine) {
-        if (this.currentMachine.earningRate) {
-          earningRate = `$${(this.currentMachine.earningRate * 86400).toFixed(3)}/day`
-        } else {
-          earningRate = '$0.000/day'
-        }
-      }
-    }
-
-    return earningRate
-  }
-
-  constructor(private readonly store: RootStore) {}
 
   @action
   setCurrentMachine = (machine: Machine) => {
