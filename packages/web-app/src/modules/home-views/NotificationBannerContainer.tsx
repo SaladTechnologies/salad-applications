1b455643efd4e20314a0b9c0a4cf3bc63a5069a7
Daniel Sarfati <danielrsarfati@gmail.com>
2019-09-15 19:44:15 -0600

    Updated banner for 0.3.0 release downtime (#177)

--- packages/web-app/src/modules/home-views/NotificationBannerContainer.tsx
+++ packages/web-app/src/modules/home-views/NotificationBannerContainer.tsx
@@ -4,9 +4,9 @@ import { NotificationBanner } from './components/NotificationBanner'
 // These values wont change when time updates, they will only be used when the component loads. In the future we could update these dynamically.
 const mapStoreToProps = () => ({
   now: new Date(),
-  startDate: new Date('July 24, 2019 16:00:00 GMT-06:00'),
-  endDate: new Date('August 25, 2019 23:59:00 GMT-06:00'),
-  text: 'Back to school sale July 24 - August 25',
+  startDate: new Date('July 24, 2018 16:00:00 GMT-06:00'),
+  endDate: new Date('August 25, 2020 23:59:00 GMT-06:00'),
+  text: 'Scheduled Downtime Monday from 11am-1pm UTC',
 })
 
 export const NotificationBannerContainer = connect(
