7d4f3fdc6f35a243c11a157ce846d6a53317a13b
Daniel Sarfati <danielrsarfati@gmail.com>
2019-10-18 14:59:46 -0600

    Added build script with sentry cli

---
+++ packages/web-app/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo 'Building web-application'
+yarn build
+
+if [ "$CONTEXT" = "production" ]
+then
+echo 'Installing sentry-cli'
+npm install @sentry/cli
+
+export SENTRY_ORG=salad-technologies
+export SENTRY_PROJECT=web-app
+export VERSION=$COMMIT_REF
+
+echo 'Creating a new sentry release'
+./node_modules/.bin/sentry-cli releases new "$VERSION"
+
+echo 'Setting release commits in sentry'
+./node_modules/.bin/sentry-cli releases set-commits "$VERSION" --commit "SaladTechnologies/salad-applications@$COMMIT_REF"
+
+echo 'Uploading source maps to sentry'
+./node_modules/.bin/sentry-cli releases files "$VERSION" upload-sourcemaps build/static/js/ --rewrite --url-prefix '~/static/js'
+
+echo 'Finalizing sentry release'
+./node_modules/.bin/sentry-cli releases finalize "$VERSION"
+
+fi
