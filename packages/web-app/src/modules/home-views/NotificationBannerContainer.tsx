7a9640a73795064544cc5e99a7cba247b5b03e39
Maria <maria@salad.io>
2019-12-02 15:15:12 -0700

    Banner update
    giving 1.5x earnings for outage

--- packages/web-app/src/modules/home-views/NotificationBannerContainer.tsx
+++ packages/web-app/src/modules/home-views/NotificationBannerContainer.tsx
@@ -4,9 +4,9 @@ import { NotificationBanner } from './components/NotificationBanner'
 // These values wont change when time updates, they will only be used when the component loads. In the future we could update these dynamically.
 const mapStoreToProps = (): any => ({
   now: new Date(),
-  startDate: new Date('July 24, 2018 16:00:00 GMT-06:00'),
-  endDate: new Date('October 11, 2019 12:00:00 GMT-06:00'),
-  text: '2X Earnings Until 10/11!',
+  startDate: new Date('December 2, 2019 16:00:00 GMT-06:00'),
+  endDate: new Date('December 4, 2019 16:00:00 GMT-06:00'),
+  text: '1.5X Earnings until 12/4',
 })
 
 export const NotificationBannerContainer = connect(
