7a786ce59993296a3f1ed931112056af80d764c2
Tim Clancy <tim@rockhopper.us>
2019-12-20 18:39:12 -0500

    Activating additional mining definitions.

--- packages/web-app/src/modules/salad-bowl/PluginDefinitionFactory.ts
+++ packages/web-app/src/modules/salad-bowl/PluginDefinitionFactory.ts
@@ -2,6 +2,11 @@ import { RootStore } from '../../Store'
 import { getClaymoreDefinition } from './definitions/getClaymoreDefinition'
 import { getGminerBeamV2Definition } from './definitions/getGminerBeamV2Definition'
 import { getGminerEthDefinition } from './definitions/getGminerEthDefinition'
+import { getXMRigCUDADefinition } from './definitions/getXMRigCUDADefinition'
+import { getXMRigOpenCLDefinition } from './definitions/getXMRigOpenCLDefinition'
+import { getCCMinerX16rDefinition } from './definitions/getCCMinerX16rDefinition'
+import { getCCMinerLyra2rev3Definition } from './definitions/getCCMinerLyra2rev3Definition'
+import { getCCMinerEquihashDefinition } from './definitions/getCCMinerEquihashDefinition'
 import { PluginDefinition } from './models'
 
 export const getPluginDefinitions = (store: RootStore): PluginDefinition[] => {
@@ -12,10 +17,26 @@ export const getPluginDefinitions = (store: RootStore): PluginDefinition[] => {
     return []
   }
 
-  return [
-    getGminerBeamV2Definition(machine),
-    getGminerEthDefinition(machine),
-    getClaymoreDefinition(machine),
-    // TODO: getEthminerDefinition(machine, machineInfo),
-  ]
+  let supportsCuda = machineInfo.graphics.controllers.some(x => x.vendor.toLocaleLowerCase().includes('nvidia'))
+  if (supportsCuda) {
+    return [
+      getGminerBeamV2Definition(machine),
+      getGminerEthDefinition(machine),
+      getClaymoreDefinition(machine),
+      getXMRigCUDADefinition(machine),
+      getCCMinerX16rDefinition(machine),
+      getCCMinerLyra2rev3Definition(machine),
+      getCCMinerEquihashDefinition(machine)
+    ]
+  } else {
+    return [
+      getGminerBeamV2Definition(machine),
+      getGminerEthDefinition(machine),
+      getClaymoreDefinition(machine),
+      getXMRigOpenCLDefinition(machine),
+      getCCMinerX16rDefinition(machine),
+      getCCMinerLyra2rev3Definition(machine),
+      getCCMinerEquihashDefinition(machine)
+    ]
+  }
 }
