1035ca1025d2d7db74ca4b6553318a6e217e2e02
Daniel Sarfati <danielrsarfati@gmail.com>
2020-02-06 14:27:45 -0700

    Added default env variables to .env

--- packages/web-app/build-storybook.sh
+++ packages/web-app/build-storybook.sh
@@ -2,6 +2,7 @@
 
 echo 'Building storybook'
 
+# Required due to a bug in storybook https://github.com/storybookjs/storybook/issues/9564
 sed -i -e 's|"homepage": "https://www.salad.io/"|"homepage": "./"|' package.json
 
 yarn run build-storybook || { echo 'build failed' ; exit 1; }
