965b8b66697d38d4e0bf41a46466a11334eb8d0e
Daniel Sarfati <danielrsarfati@gmail.com>
2020-02-27 08:17:51 -0700

    Added status & toggle text to start button (#305)

--- packages/web-app/src/modules/machine-views/StartButtonContainer.ts
+++ packages/web-app/src/modules/machine-views/StartButtonContainer.ts
@@ -3,14 +3,12 @@ import { RootStore } from '../../Store'
 import { StartButton } from './components/StartButton'
 
 const mapStoreToProps = (store: RootStore): any => ({
-  balance: store.balance.currentBalance,
+  currentBalance: store.balance.currentBalance,
   lifetimeBalance: store.balance.lifetimeBalance,
   onClick: store.saladBowl.toggleRunning,
   isRunning: store.saladBowl.isRunning,
   startEnabled: store.saladBowl.canRun,
+  miningStatus: store.saladBowl.status,
 })
 
-export const StartButtonContainer = connect(
-  mapStoreToProps,
-  StartButton,
-)
+export const StartButtonContainer = connect(mapStoreToProps, StartButton)
