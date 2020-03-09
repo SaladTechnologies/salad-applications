b5b33fb84f3462254cbaeb5f1468a510f5fef27c
Daniel Sarfati <danielrsarfati@gmail.com>
2020-03-09 11:44:14 -0600

    Added "Explore All" button to each reward category

--- packages/web-app/src/modules/reward-views/pages/RewardPages.stories.tsx
+++ packages/web-app/src/modules/reward-views/pages/RewardPages.stories.tsx
@@ -57,34 +57,57 @@ storiesOf('Modules/Reward Pages/Reward Details Page', module)
   })
 
 storiesOf('Modules/Reward Pages/Browse Rewards Page', module)
-  .addDecorator(storyFn => (
-    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>{storyFn()}</div>
-  ))
   .add('with rewards (n=1)', () => {
     let rewards = generateRewards(1)
-    return <BrowseRewardsPage rewards={rewards} onViewReward={action('view reward')} />
+    return <BrowseRewardsPage rewards={rewards} onViewReward={action('view reward')} onBack={action('back')} />
   })
   .add('with rewards (n=4)', () => {
     let rewards = generateRewards(4)
-    return <BrowseRewardsPage rewards={rewards} onViewReward={action('view reward')} />
+    return <BrowseRewardsPage rewards={rewards} onViewReward={action('view reward')} onBack={action('back')} />
   })
   .add('with rewards (n=10)', () => {
     let rewards = generateRewards(10)
-    return <BrowseRewardsPage rewards={rewards} onViewReward={action('view reward')} />
+    return <BrowseRewardsPage rewards={rewards} onViewReward={action('view reward')} onBack={action('back')} />
   })
   .add('with rewards & title (n=50)', () => {
     let rewards = generateRewards(50)
-    return <BrowseRewardsPage title={'Amazing Games'} rewards={rewards} onViewReward={action('view reward')} />
+    return (
+      <BrowseRewardsPage
+        title={'Amazing Games'}
+        rewards={rewards}
+        onViewReward={action('view reward')}
+        onBack={action('back')}
+      />
+    )
   })
   .add('no rewards w/ title', () => {
-    return <BrowseRewardsPage title={'Amazing Games'} rewards={undefined} onViewReward={action('view reward')} />
+    return (
+      <BrowseRewardsPage
+        title={'Amazing Games'}
+        rewards={undefined}
+        onViewReward={action('view reward')}
+        onBack={action('back')}
+      />
+    )
   })
   .add('no rewards (undefined)', () => {
-    return <BrowseRewardsPage rewards={undefined} onViewReward={action('view reward')} />
+    return <BrowseRewardsPage rewards={undefined} onViewReward={action('view reward')} onBack={action('back')} />
   })
   .add('no rewards (empty)', () => {
-    return <BrowseRewardsPage rewards={[]} onViewReward={action('view reward')} />
+    return <BrowseRewardsPage rewards={[]} onViewReward={action('view reward')} onBack={action('back')} />
   })
   .add('with title', () => {
-    return <BrowseRewardsPage title={'Amazing Games'} rewards={[]} onViewReward={action('view reward')} />
+    return (
+      <BrowseRewardsPage
+        title={'Amazing Games'}
+        rewards={[]}
+        onViewReward={action('view reward')}
+        onBack={action('back')}
+      />
+    )
+  })
+  .add('no title', () => {
+    return (
+      <BrowseRewardsPage title={undefined} rewards={[]} onViewReward={action('view reward')} onBack={action('back')} />
+    )
   })
