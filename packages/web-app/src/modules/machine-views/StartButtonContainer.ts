7ea4f6b3063a58490e0d03aee156fc76a700c53e
Daniel Sarfati <danielrsarfati@gmail.com>
2020-03-11 14:58:25 -0600

    Added earning rate label to start button

--- packages/web-app/src/modules/machine-views/StartButtonContainer.ts
+++ packages/web-app/src/modules/machine-views/StartButtonContainer.ts
@@ -5,6 +5,7 @@ import { StartButton } from './components/StartButton'
 const mapStoreToProps = (store: RootStore): any => ({
   currentBalance: store.balance.currentBalance,
   lifetimeBalance: store.balance.lifetimeBalance,
+  earningRate: store.machine.currentEarningRate,
   onClick: store.saladBowl.toggleRunning,
   isRunning: store.saladBowl.isRunning,
   startEnabled: store.saladBowl.canRun,
