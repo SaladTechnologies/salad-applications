af679c7da07ff425cc831e2bd899b1bc8c48079f
Damon Sorensen <damonsorensen@gmail.com>
2020-03-16 15:50:57 -0600

    CloseWindow off of canMinimize instead of minimize (#336)
    * CloseWindow off of canMinimize instead of minimize
    
    * Requested Changes

--- packages/web-app/src/modules/machine/NativeStore.ts
+++ packages/web-app/src/modules/machine/NativeStore.ts
@@ -213,7 +213,7 @@ export class NativeStore {
 
   @action
   closeWindow = () => {
-    if (this.minimizeToTray) {
+    if (this.canMinimizeToTray && this.minimizeToTray) {
       this.send(hide)
     } else {
       this.send(close)
