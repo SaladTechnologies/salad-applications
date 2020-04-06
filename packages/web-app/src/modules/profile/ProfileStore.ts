6da9b25cf3a466307eead88e5d1e5b621ce633d5
Daniel Sarfati <danielrsarfati@gmail.com>
2020-04-06 09:46:10 -0600

    Added EngagementStore to show notifications to help new users (#363)

--- packages/web-app/src/modules/profile/ProfileStore.ts
+++ packages/web-app/src/modules/profile/ProfileStore.ts
@@ -1,4 +1,4 @@
-import { observable, action, flow, computed } from 'mobx'
+import { observable, action, flow } from 'mobx'
 import { Profile } from './models'
 import { Config } from '../../config'
 import { RootStore } from '../../Store'
@@ -17,14 +17,6 @@ export class ProfileStore {
   @observable
   public isLoading: boolean = false
 
-  @computed
-  public get showWhatsNew(): boolean {
-    const show =
-      this.currentProfile !== undefined && this.currentProfile.lastSeenApplicationVersion !== Config.whatsNewVersion
-
-    return show
-  }
-
   constructor(private readonly store: RootStore, private readonly axios: AxiosInstance) {}
 
   @action.bound
@@ -37,16 +29,6 @@ export class ProfileStore {
       this.currentProfile = profile.data
       //TODO: Move the routing logic to the onLogin function so we can load all the data before showing the app
       this.store.routing.replace('/')
-
-      //Check to see if we should add the What's New notification
-      if (this.showWhatsNew) {
-        this.store.notifications.sendNotification({
-          title: 'Salad was Updated',
-          message: 'Something new just came out from the kitchen. Click here to learn more.',
-          autoClose: false,
-          onClick: () => this.store.ui.showModal('/whats-new'),
-        })
-      }
     } catch (err) {
       console.error('Profile error: ', err)
       this.currentProfile = undefined
