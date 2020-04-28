88bc66c3053143f34d44eadabc011afa45d13d7b
Timothy Clancy <tim@rockhopper.us>
2020-04-28 12:48:58 -0400

    Added parameters to attempt to mitigate 4GB VRAM requirements.
    This lets users optimistically try to run the mining clients even if they fail the check for the initial VRAM resolution.

--- packages/web-app/src/modules/salad-bowl/definitions/getClaymoreEthashDefinition.ts
+++ packages/web-app/src/modules/salad-bowl/definitions/getClaymoreEthashDefinition.ts
@@ -10,7 +10,7 @@ export const getClaymoreEthashDefinition = (machine: Machine): PluginDefinition 
     name: 'Claymore',
     downloadUrl: 'https://github.com/SaladTechnologies/plugin-downloads/releases/download/claymore15/claymore-15-windows.zip',
     exe: 'EthDcrMiner64.exe',
-    args: `${claymoreRegion('usa')} ${claymoreRegion('eu')} -ewal ${NICEHASH_MINING_ADDRESS}.${machine.minerId} -esm 3 -allpools 1 -allcoins 0`,
+    args: `${claymoreRegion('usa')} ${claymoreRegion('eu')} -ewal ${NICEHASH_MINING_ADDRESS}.${machine.minerId} -eres 0 -esm 3 -allpools 1 -allcoins 0`,
     runningCheck: '(?:Share accepted|[1-9][0-9]*.d* (?:kh|kH|Kh|KH|mh|mH|Mh|MH)/s)',
     initialTimeout: 600000,
     initialRetries: 3,
