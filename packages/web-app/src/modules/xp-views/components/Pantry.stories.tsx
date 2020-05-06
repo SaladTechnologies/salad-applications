3f742b2f4297b108831f0be2f2af4e2c7e76afdb
Daniel Sarfati <danielrsarfati@gmail.com>
2020-05-06 14:57:18 -0600

    Added new veggies, re-balanced XP

--- packages/web-app/src/modules/xp-views/components/Pantry.stories.tsx
+++ packages/web-app/src/modules/xp-views/components/Pantry.stories.tsx
@@ -8,19 +8,19 @@ export default {
   component: Pantry,
 }
 
-const levels = defaultLevels
+const levels = defaultLevels()
 
 export const Basic = () => {
   let xp = number('Current XP', 0, {
     range: true,
     min: 0,
-    max: 250000,
+    max: 150000,
     step: 1,
   })
 
   let options: any = {}
   for (var l of levels) {
-    options[l.title] = l
+    options[l.key] = l
   }
 
   return <Pantry currentXp={xp} levels={levels} />
