50de0341c8d74b56ebe78eab0c5bc2323534f416
Daniel Sarfati <danielrsarfati@gmail.com>
2020-05-14 10:49:24 -0600

    Added start button to footer bar (#423)

--- packages/web-app/src/modules/home-views/FooterBarContainer.tsx
+++ packages/web-app/src/modules/home-views/FooterBarContainer.tsx
@@ -3,6 +3,7 @@ import { RootStore } from '../../Store'
 import { FooterBar } from './components/FooterBar'
 
 const mapStoreToProps = (store: RootStore): any => ({
+  onToggleMining: store.saladBowl.toggleRunning,
   status: store.saladBowl.status,
 })
 
