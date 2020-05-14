b0f0a689486079a2fbc55ba748b1d5a789fffd25
Daniel Sarfati <danielrsarfati@gmail.com>
2020-05-14 10:48:47 -0600

    Added start button to footer bar

--- packages/web-app/src/modules/home-views/components/FooterBar.tsx
+++ packages/web-app/src/modules/home-views/components/FooterBar.tsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import withStyles, { WithStyles } from 'react-jss'
 import { SaladTheme } from '../../../SaladTheme'
 import { SmartLink } from '../../../components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons'
+import { MiningStatus } from '../../machine/models'
 
 export class MenuItem {
   constructor(public readonly name: string, public readonly url: string, public readonly showNotification?: boolean) {}
@@ -23,19 +26,36 @@ const styles = (theme: SaladTheme) => ({
   statusText: {
     textTransform: 'uppercase',
   },
+  button: {
+    padding: '0px 10px',
+    cursor: 'pointer',
+    '&:hover': {
+      color: theme.lightGreen,
+    },
+  },
 })
 
 interface Props extends WithStyles<typeof styles> {
   status?: string
+  onToggleMining?: () => void
 }
 
 class _FooterBar extends Component<Props> {
+  handleClick = () => {
+    const { onToggleMining } = this.props
+
+    onToggleMining?.()
+  }
+
   render() {
     const { status, classes } = this.props
     return (
       <div className={classes.container}>
+        <div className={classes.button} onClick={this.handleClick}>
+          {status === MiningStatus.Stopped ? <FontAwesomeIcon icon={faPlay} /> : <FontAwesomeIcon icon={faStop} />}
+        </div>
         <div className={classes.statusText}>
-          <SmartLink to="/earn/mine">{status}</SmartLink>
+          Status: <SmartLink to="/earn/mine">{status}</SmartLink>
         </div>
       </div>
     )
