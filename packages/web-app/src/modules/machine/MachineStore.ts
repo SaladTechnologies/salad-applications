5cc44313cac4c470ab1003e919995e3690b9ac9c
Daniel Sarfati <danielrsarfati@gmail.com>
2020-06-02 00:28:51 -0600

    Updated machine registration API (#439)

--- packages/web-app/src/modules/machine/MachineStore.ts
+++ packages/web-app/src/modules/machine/MachineStore.ts
@@ -1,6 +1,7 @@
-import { action, observable, computed } from 'mobx'
-import { Machine } from './models/Machine'
+import { AxiosInstance } from 'axios'
+import { autorun, computed, flow, observable } from 'mobx'
 import { RootStore } from '../../Store'
+import { Machine } from './models/Machine'
 
 export class MachineStore {
   @observable
@@ -15,10 +16,37 @@ export class MachineStore {
     }
   }
 
-  constructor(private readonly store: RootStore) {}
+  constructor(private readonly store: RootStore, private readonly axios: AxiosInstance) {
+    autorun(async () => {
+      if (!store.auth.isAuthenticated) {
+        return
+      }
 
-  @action
-  setCurrentMachine = (machine: Machine) => {
-    this.currentMachine = machine
+      if (!store.native.machineInfo) {
+        return
+      }
+
+      await this.registerMachine()
+    })
   }
+
+  registerMachine = flow(
+    function* (this: MachineStore) {
+      if (!this.store.native.machineInfo) {
+        console.warn('No valid machine info found. Unable to register.')
+        return
+      }
+
+      try {
+        console.log('Registering machine with salad')
+        let req = { systemInfo: this.store.native.machineInfo }
+        let res: any = yield this.axios.post(`/api/v2/machines`, req)
+        let machine: Machine = res.data
+        this.currentMachine = machine
+      } catch (err) {
+        this.store.analytics.captureException(new Error(`register-machine error: ${err}`))
+        throw err
+      }
+    }.bind(this),
+  )
 }
