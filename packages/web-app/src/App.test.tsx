eb681468ef2eeb3d2f28f84ff7eaf1b42c65adf9
Kyle Dodson <kyledodson@gmail.com>
2020-08-14 20:51:45 -0700

    Update npm dependencies (#489)

--- packages/web-app/src/App.test.tsx
+++ packages/web-app/src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createMemoryHistory } from 'history';
+
+const history = createMemoryHistory();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App history={history} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
