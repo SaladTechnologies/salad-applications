40821ebe1903d0df5e73f9a35cc50c95242ffb68
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-08-23 19:30:54 +0200

    update purchase successful message

--- packages/web-app/src/modules/reward/RewardStore.ts
+++ packages/web-app/src/modules/reward/RewardStore.ts
@@ -254,8 +254,7 @@ export class RewardStore {
       //Show a notification
       this.store.notifications.sendNotification({
         title: `You redeemed ${reward.name}!`,
-        message: `Congrats on your pick! Your reward is available in the reward vault!`,
-        onClick: () => this.store.routing.push('/account/reward-vault'),
+        message: `Congrats on your pick! We've sent your reward to your email.`,
         autoClose: false,
       })
     } catch (error) {
