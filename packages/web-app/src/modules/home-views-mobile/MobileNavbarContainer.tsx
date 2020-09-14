86589f64317f7a79e4d6fdd85c27c3c190fc9d8b
Daniel Sarfati <danielrsarfati@gmail.com>
2020-09-14 14:28:14 -0600

    Highlight route on mobile nav bar

--- packages/web-app/src/modules/home-views-mobile/MobileNavbarContainer.tsx
+++ packages/web-app/src/modules/home-views-mobile/MobileNavbarContainer.tsx
@@ -1,14 +1,15 @@
 import { faQuestionCircle, faUser } from '@fortawesome/free-regular-svg-icons'
-import { faHome, faListUl } from '@fortawesome/free-solid-svg-icons'
+import { faDollarSign, faListUl } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { connect } from '../../connect'
+import { RootStore } from '../../Store'
 import { MobileNavbar, MobileNavItem } from './components/MobileNavbar'
 
 const size = '3x'
 
 const menuItems: MobileNavItem[] = [
-  new MobileNavItem('Earnings', <FontAwesomeIcon size={size} icon={faHome} />, '/earn/summary'),
+  new MobileNavItem('Earnings', <FontAwesomeIcon size={size} icon={faDollarSign} />, '/earn/summary'),
   new MobileNavItem('Offerwalls', <FontAwesomeIcon size={size} icon={faListUl} />, '/earn/offerwall'),
   new MobileNavItem('Account', <FontAwesomeIcon size={size} icon={faUser} />, '/account/summary'),
   new MobileNavItem('Support', <FontAwesomeIcon size={size} icon={faQuestionCircle} />, undefined, () => {
@@ -20,7 +21,8 @@ const menuItems: MobileNavItem[] = [
   }),
 ]
 
-const mapStoreToProps = (): any => ({
+const mapStoreToProps = (store: RootStore): any => ({
+  path: store.routing.location, //Used to trigger the component to render with the current location
   navItems: menuItems,
 })
 
