4b50394192265bbf64581c73e9f594a7e91c9742
Krish120003 <krish120003@gmail.com>
2020-09-15 20:22:45 -0400

    Add MacOS dock icon paths

--- packages/desktop-app/src/icons.ts
+++ packages/desktop-app/src/icons.ts
@@ -19,6 +19,11 @@ export const TRAY_ALERT_ICON_PATH = path.join(
   process.platform === 'win32' ? 'logo-alert.ico' : 'logo-alert.png',
 )
 
+// For MacOS Dock
+export const DOCK_LOGO_ACTIVE_PATH = path.join(__static, 'logo-active.png')
+// For MacOS Dock
+export const DOCK_LOGO_ALERT_PATH = path.join(__static, 'logo-alert.png')
+
 export const TASKBAR_OVERLAY_ACTIVE_PATH = path.join(__static, 'taskbar-overlay-active.png')
 
 export const NOTIFICATION_ICON_PATH = path.join(__static, 'logo.png')
