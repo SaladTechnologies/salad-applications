5401e71540ea6a0bd1672e709306693dca28bb9a
Krish120003 <krish120003@gmail.com>
2020-09-15 20:05:09 -0400

    Create icons.ts

---
+++ packages/desktop-app/src/icons.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+import process from 'process'
+
+declare const __static: string
+
+export const LOGO_PATH = path.join(__static, process.platform === 'win32' ? 'logo.ico' : 'logo.png')
+
+export const LOGO_ANIMATED_PATH = path.join(__static, 'logo.gif')
+
+export const TRAY_ICON_PATH = LOGO_PATH
+
+export const TRAY_ACTIVE_ICON_PATH = path.join(
+  __static,
+  process.platform === 'win32' ? 'logo-active.ico' : 'logo-active.png',
+)
+
+export const TRAY_ALERT_ICON_PATH = path.join(
+  __static,
+  process.platform === 'win32' ? 'logo-alert.ico' : 'logo-alert.png',
+)
+
+export const TASKBAR_OVERLAY_ACTIVE_PATH = path.join(__static, 'taskbar-overlay-active.png')
+
+export const NOTIFICATION_ICON_PATH = path.join(__static, 'logo.png')
