aae019ac2e5747d7c9038264a67116eaf4a0ee98
Daniel Sarfati <danielrsarfati@gmail.com>
2020-09-16 11:09:44 -0700

    Fixes to mobile breakpoints (#528)
    * Hide mobile layouts in desktop app
    
    * Updated responsive breakpoints to ignore mobile keyboards

--- packages/web-app/src/components/DeviceTypes.tsx
+++ packages/web-app/src/components/DeviceTypes.tsx
@@ -9,9 +9,9 @@ interface Props {
 const mobileSize = 812
 
 export const MobileDevice = ({ orientation, children }: Props) => (
-  <MediaQuery maxWidth={mobileSize} orientation={orientation}>
+  <MediaQuery maxDeviceWidth={mobileSize} orientation={orientation}>
     {children}
   </MediaQuery>
 )
 
-export const NotMobile = ({ children }: Props) => <MediaQuery minWidth={mobileSize + 1}>{children}</MediaQuery>
+export const NotMobile = ({ children }: Props) => <MediaQuery minDeviceWidth={mobileSize + 1}>{children}</MediaQuery>
