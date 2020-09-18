62151df357f81b77fac6613018f72b81037ee827
Kyle Dodson <kyledodson@gmail.com>
2020-09-18 13:18:23 -0700

    Add macOS installer and improve dock/menu bar integration

--- packages/desktop-app/src/icons.ts
+++ packages/desktop-app/src/icons.ts
@@ -1,27 +1,29 @@
 import path from 'path'
 import process from 'process'
 
+// TODO: Move this to a separate `.d.ts` declaration file. This is injected by `electron-webpack`.
 declare const __static: string
 
-export const LOGO_PATH = path.join(__static, process.platform === 'win32' ? 'logo.ico' : 'logo.png')
+export const DOCK_ACTIVE_ICON_PATH = path.join(__static, 'logo-active.png')
 
-export const TRAY_ICON_PATH = LOGO_PATH
+export const DOCK_ICON_PATH = path.join(__static, 'logo.png')
+
+export const NOTIFICATION_ICON_PATH = path.join(__static, 'logo.png')
+
+export const TASKBAR_ACTIVE_OVERLAY_ICON_PATH = path.join(__static, 'taskbar-overlay-active.png')
 
 export const TRAY_ACTIVE_ICON_PATH = path.join(
   __static,
-  process.platform === 'win32' ? 'logo-active.ico' : 'logo-active.png',
+  process.platform === 'darwin'
+    ? 'menu-bar-active.png'
+    : process.platform === 'win32'
+    ? 'logo-active.ico'
+    : 'logo-active.png',
 )
 
-export const TRAY_ALERT_ICON_PATH = path.join(
+export const TRAY_ICON_PATH = path.join(
   __static,
-  process.platform === 'win32' ? 'logo-alert.ico' : 'logo-alert.png',
+  process.platform === 'darwin' ? 'menu-bar.png' : process.platform === 'win32' ? 'logo.ico' : 'logo.png',
 )
 
-// [Placeholder] For MacOS Dock
-export const DOCK_LOGO_ACTIVE_PATH = path.join(__static, 'logo-active.png')
-// [Placeholder] For MacOS Dock
-export const DOCK_LOGO_ALERT_PATH = path.join(__static, 'logo-alert.png')
-
-export const TAKSBAR_ACTIVE_OVERLAY_ICON_PATH = path.join(__static, 'taskbar-overlay-active.png')
-
-export const NOTIFICATION_ICON_PATH = path.join(__static, 'logo.png')
+export const WINDOW_ICON_PATH = path.join(__static, process.platform === 'win32' ? 'logo.ico' : 'logo.png')
