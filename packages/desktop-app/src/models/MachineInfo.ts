62aef1313f7d3f31afff2343f54c164b4b22a673
Krish120003 <krish120003@gmail.com>
2020-09-25 23:07:16 -0400

    Update MachineInfo.ts model to optionally use LilnuxGraphicsData for graphics

--- packages/desktop-app/src/models/MachineInfo.ts
+++ packages/desktop-app/src/models/MachineInfo.ts
@@ -1,8 +1,9 @@
 import * as si from 'systeminformation'
+import { LinuxGraphicsData } from '../salad-bowl/models/LinuxGraphics'
 
 export interface MachineInfo {
   cpu?: si.Systeminformation.CpuData | si.Systeminformation.CpuWithFlagsData
-  graphics?: si.Systeminformation.GraphicsData
+  graphics?: si.Systeminformation.GraphicsData | LinuxGraphicsData
   memLayout?: si.Systeminformation.MemLayoutData[]
   os?: si.Systeminformation.OsData
   platform?: 'aix' | 'android' | 'cygwin' | 'darwin' | 'freebsd' | 'linux' | 'netbsd' | 'openbsd' | 'sunos' | 'win32'
