815b9c6ac0f70a1bf029946eeb241566386cbe79
Kyle Dodson <kyledodson@gmail.com>
2020-10-05 16:50:14 -0700

    Disable GPU mining on macOS

--- packages/web-app/src/modules/salad-bowl/definitions/macos/xmrig-randomx.tsx
+++ packages/web-app/src/modules/salad-bowl/definitions/macos/xmrig-randomx.tsx
@@ -1,7 +1,7 @@
 import { Accounts } from '../accounts'
 import { STANDARD_ERRORS } from '../errors'
 import { PluginDefinition } from '../plugin-definitions'
-import { hasCpu, hasGpu, not } from '../requirements'
+import { hasCpu } from '../requirements'
 import { downloads } from '../xmrig'
 
 export const createXMRigRandomXPluginDefinitions = (accounts: Accounts): PluginDefinition[] =>
@@ -10,20 +10,21 @@ export const createXMRigRandomXPluginDefinitions = (accounts: Accounts): PluginD
       `-o stratum+tcp://randomxmonero.${location}.nicehash.com:3380 --coin=monero -u ${accounts.nicehash.address}.${accounts.nicehash.rigId} -k --nicehash`
 
     if (download.macOSUrl !== undefined) {
-      definitions.push({
-        name: 'XMRig',
-        version: download.version,
-        algorithm: 'RandomX',
-        downloadUrl: download.macOSUrl,
-        exe: 'xmrig',
-        args: `--no-cpu --cuda --opencl --donate-level=1 ${connection('usa')} ${connection('eu')}`,
-        runningCheck: '(?:accepted|[1-9][0-9]*\\.\\d* H\\/s)',
-        initialTimeout: 600000,
-        initialRetries: 3,
-        watchdogTimeout: 900000,
-        errors: [...STANDARD_ERRORS],
-        requirements: [not(hasGpu('*', 4096)), hasGpu('*', 2048)],
-      })
+      // TODO: Enable GPU mining on macOS
+      //// definitions.push({
+      ////   name: 'XMRig',
+      ////   version: download.version,
+      ////   algorithm: 'RandomX',
+      ////   downloadUrl: download.macOSUrl,
+      ////   exe: 'xmrig',
+      ////   args: `--no-cpu --cuda --opencl --donate-level=1 ${connection('usa')} ${connection('eu')}`,
+      ////   runningCheck: '(?:accepted|[1-9][0-9]*\\.\\d* H\\/s)',
+      ////   initialTimeout: 600000,
+      ////   initialRetries: 3,
+      ////   watchdogTimeout: 900000,
+      ////   errors: [...STANDARD_ERRORS],
+      ////   requirements: [not(hasGpu('*', 4096)), hasGpu('*', 2048)],
+      //// })
 
       definitions.push({
         name: 'XMRig-CPU',
