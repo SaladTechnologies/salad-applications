d589e10059a4658e2616e788540865cc18f64c6d
Scott Cinatl <scottcinatl@gmail.com>
2020-11-18 16:48:32 -0800

    Updated logic to initialize Zendesk settings on load than trigger re-authentication

--- packages/web-app/src/modules/zendesk/Zendesk.ts
+++ packages/web-app/src/modules/zendesk/Zendesk.ts
@@ -1,29 +1,35 @@
 import { AxiosInstance } from 'axios'
 import { RootStore } from '../../Store'
-// import {Window} from '../zendesk/models'
 import { Profile } from '../profile/models'
 
 export class Zendesk {
-  constructor(private store: RootStore, private readonly axios: AxiosInstance) {}
+  constructor(private store: RootStore, private readonly axios: AxiosInstance) { }
 
   private async getJWTToken(): Promise<string | undefined> {
     let jwtToken: string | undefined = undefined
     try {
       let response = await this.axios.post(`/api/v2/zendesk-tokens`);
       jwtToken = response.data.token
-    } catch (err) {
-      console.log("Unable to retrieve JWT for Zendesk Authentication")
-      console.log(err)
+    } catch (e) {
+      console.log("Unable to retrieve JWT for Zendesk Authentication");
+      console.log(e);
     }
 
     return jwtToken
   }
 
-   intializeZendesk() {
+  async intializeZendesk() {
     if (typeof window !== "undefined") {
-    //@ts-ignore
-    window.zeSettings = {
-      webWidget: {
+      let token = await this.getJWTToken()
+      window.zESettings = {
+        webWidget: {
+          authenticate: {
+            jwtFn: function (callback) {
+              if (token) {
+                callback(token);
+              }
+            }
+          },
           offset: {
             mobile: {
               horizontal: '-530px',
@@ -31,7 +37,7 @@ export class Zendesk {
             }
           }
         }
-    }
+      }
 
       // Append Zendesk snippet to body tag.
       const zendeskSnippetScript = document.createElement("script");
@@ -45,40 +51,33 @@ export class Zendesk {
 
   authenticateUser(profile: Profile) {
     if (this.store.auth.isAuthenticated) {
-      let token = this.getJWTToken();
-      if (token) {
-      //@ts-ignore
-      window.zeSettings.webWidget.authenticate = {
-        //@ts-ignore
-        jwtFn: function(callback) {
-          callback(token);
-            }
-        }
+      // Trigger reauthentication after web widget page load.
+      window.zE && window.zE('webWidget', 'helpCenter:reauthenticate');
 
-        try {
-          //@ts-ignore
-          window.zE && zE('webWidget', 'prefill', {
-            name: {
-              value: profile.username,
-              readOnly: true, // optional
-            },
-            email: {
-              value: profile.email.toLocaleLowerCase(),
-              readOnly: true, // optional
-            },
-          })
-        } catch (e) {
-          console.error('Unable to prefill Zendesk')
-          console.error(e)
-        }
+      try {
+        window.zE && window.zE('webWidget', 'prefill', {
+          name: {
+            value: profile.username,
+            readOnly: true, // optional
+          },
+          email: {
+            value: profile.email.toLocaleLowerCase(),
+            readOnly: true, // optional
+          },
+        })
+      } catch (e) {
+        console.error('Unable to prefill Zendesk')
+        console.error(e)
       }
     }
   }
 
+
+
   logout() {
     try {
-      //@ts-ignore
-      window.zE && zE.logout()
+      // @ts-ignore
+      window.zE && window.zE.logout()
     } catch (e) {
       console.error('Unable to logout of Zendesk')
       console.error(e)
