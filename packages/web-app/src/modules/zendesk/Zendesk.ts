9532ea4828347c41a1843e5437931d7a191532b6
Scott Cinatl <scottcinatl@gmail.com>
2020-11-30 13:34:01 -0800

    Added retry logic for failed authentication attempts

--- packages/web-app/src/modules/zendesk/Zendesk.ts
+++ packages/web-app/src/modules/zendesk/Zendesk.ts
@@ -3,20 +3,31 @@ import { AuthStore } from '../auth'
 
 export class Zendesk {
   private static injected: boolean = false
+  private static injectionError: boolean = false
+  private initializeRetryTimeout?: NodeJS.Timeout
+  private reauthenticateRetryTimeout?: NodeJS.Timeout
 
   constructor(private readonly axios: AxiosInstance, private readonly auth: AuthStore) {
     this.inject()
   }
 
   private async getJWTToken(): Promise<string | undefined> {
-    let jwtToken: string | undefined = undefined
+    if (this.reauthenticateRetryTimeout) {
+      clearTimeout(this.reauthenticateRetryTimeout)
+      this.reauthenticateRetryTimeout = undefined
+    }
+
+    let jwtToken: string | undefined
     if (this.auth.isAuthenticated) {
       try {
         let response = await this.axios.post(`/api/v2/zendesk-tokens`)
         jwtToken = response.data.token
       } catch (e) {
-        console.log('Unable to retrieve JWT for Zendesk Authentication')
-        console.log(e)
+        if (e.response == null || e.response.status !== 404) {
+          this.reauthenticateRetryTimeout = setTimeout(() => {
+            window.zE!('webWidget', 'helpCenter:reauthenticate')
+          }, 30000)
+        }
       }
     }
 
@@ -29,7 +40,7 @@ export class Zendesk {
         return
       }
 
-      // Add `zESettings` before injecting the web widget script.
+      // Add `zESettings` before injecting the Zendesk script.
       window.zESettings = {
         webWidget: {
           authenticate: {
@@ -52,6 +63,9 @@ export class Zendesk {
       // Append Zendesk script to body.
       const zendeskSnippetScript = document.createElement('script')
       zendeskSnippetScript.async = true
+      zendeskSnippetScript.onerror = () => {
+        Zendesk.injectionError = true
+      }
       zendeskSnippetScript.id = 'ze-snippet'
       zendeskSnippetScript.src = 'https://static.zdassets.com/ekr/snippet.js?key=36be7184-2a3f-4bec-9bb2-758e7c9036d0'
       document.body.appendChild(zendeskSnippetScript)
@@ -62,6 +76,18 @@ export class Zendesk {
 
   login(username: string, email: string) {
     if (!window.zE) {
+      if (this.initializeRetryTimeout !== undefined) {
+        clearTimeout(this.initializeRetryTimeout)
+        this.initializeRetryTimeout = undefined
+      }
+
+      if (!Zendesk.injectionError) {
+        this.initializeRetryTimeout = setTimeout(() => {
+          this.initializeRetryTimeout = undefined
+          this.login(username, email);
+        }, 1000);
+      }
+
       return
     }
 
@@ -83,6 +109,10 @@ export class Zendesk {
 
     try {
       window.zE('webWidget', 'helpCenter:reauthenticate')
+      if (this.initializeRetryTimeout !== undefined) {
+        clearTimeout(this.initializeRetryTimeout)
+        this.initializeRetryTimeout = undefined
+      }
     } catch (e) {
       console.log('Unable to authenticate Zendesk web widget')
       console.log(e)
@@ -91,11 +121,27 @@ export class Zendesk {
 
   logout() {
     if (!window.zE) {
+      if (this.initializeRetryTimeout !== undefined) {
+        clearTimeout(this.initializeRetryTimeout)
+        this.initializeRetryTimeout = undefined
+      }
+
+      if (!Zendesk.injectionError) {
+        this.initializeRetryTimeout = setTimeout(() => {
+          this.initializeRetryTimeout = undefined
+          this.logout();
+        }, 1000);
+      }
+
       return
     }
 
     try {
       window.zE.logout()
+      if (this.initializeRetryTimeout !== undefined) {
+        clearTimeout(this.initializeRetryTimeout)
+        this.initializeRetryTimeout = undefined
+      }
     } catch (e) {
       console.error('Unable to logout of Zendesk')
       console.error(e)
