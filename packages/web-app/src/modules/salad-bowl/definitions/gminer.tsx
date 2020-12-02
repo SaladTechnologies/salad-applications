7bfbff806c10b636ee46a331de8c846b604591d0
Kyle Dodson <kyledodson@gmail.com>
2020-12-01 20:30:22 -0800

    Update miners

--- packages/web-app/src/modules/salad-bowl/definitions/gminer.tsx
+++ packages/web-app/src/modules/salad-bowl/definitions/gminer.tsx
@@ -2,6 +2,11 @@ const baseUrl = 'https://github.com/SaladTechnologies/plugin-downloads/releases/
 
 export const downloads = [
   {
+    version: '2.33',
+    linuxUrl: baseUrl + '/gminer-2.33/gminer-2-33-linux.tar.gz',
+    windowsUrl: baseUrl + '/gminer-2.33/gminer-2-33-windows.zip',
+  },
+  {
     version: '2.23',
     linuxUrl: baseUrl + '/gminer-2.23/gminer-2-23-linux.tar.gz',
     windowsUrl: baseUrl + '/gminer-2.23/gminer-2-23-windows.zip',
@@ -9,9 +14,5 @@ export const downloads = [
   {
     version: '2.21',
     windowsUrl: baseUrl + '/gminer2.21/gminer-2-21-windows.zip',
-  },
-  {
-    version: '2.15',
-    windowsUrl: baseUrl + '/gminer2.15/gminer-2-15-windows.zip',
   },
 ]
--- packages/web-app/src/modules/salad-bowl/definitions/phoenix-miner.tsx
+++ packages/web-app/src/modules/salad-bowl/definitions/phoenix-miner.tsx
@@ -2,6 +2,11 @@ const baseUrl = 'https://github.com/SaladTechnologies/plugin-downloads/releases/
 
 export const downloads = [
   {
+    version: '5.3b',
+    linuxUrl: baseUrl + '/phoenixminer-5-3b/phoenixminer-5-3b-linux.tar.gz',
+    windowsUrl: baseUrl + '/phoenixminer-5-3b/phoenixminer-5-3b-windows.zip',
+  },
+  {
     version: '5.1c',
     linuxUrl: baseUrl + '/phoenixminer-5-1-c/phoenixminer-5-1-c-linux.tar.gz',
     windowsUrl: baseUrl + '/phoenixminer-5-1-c/phoenixminer-5-1-c-windows.zip',
@@ -9,9 +14,5 @@ export const downloads = [
   {
     version: '5.0e',
     windowsUrl: baseUrl + '/phoenixminer-5-0-e/phoenixminer-5-0-e-windows.zip',
-  },
-  {
-    version: '4.9c',
-    windowsUrl: baseUrl + '/phoenixminer-4-9-c/phoenixminer-4-9-c-windows.zip',
   },
 ]
