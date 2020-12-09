c4cd4116946818d4f75bbd5728b37f785a257ab0
Scott Cinatl <scottcinatl@gmail.com>
2020-12-09 10:03:09 -0800

    Added 404 Page for routes that don't exist on the mobile app (#583)

---
+++ packages/web-app/src/components/MobilePageNotFound.stories.tsx
@@ -0,0 +1,7 @@
+import { storiesOf } from '@storybook/react'
+import React from 'react'
+import { MobilePageNotFound } from './MobilePageNotFound'
+
+storiesOf('Components/MobilePageNotFound', module).add('MobilePageNotFound', () => {
+  return <MobilePageNotFound />
+})
