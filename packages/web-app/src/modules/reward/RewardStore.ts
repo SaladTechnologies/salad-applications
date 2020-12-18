d2c8b2a18e7c931b61937b5183b3dc071a367f2e
Daniel Sarfati <danielrsarfati@gmail.com>
2020-12-18 13:50:30 -0700

    Added "winter greens" category to the top of the storefront (#594)

--- packages/web-app/src/modules/reward/RewardStore.ts
+++ packages/web-app/src/modules/reward/RewardStore.ts
@@ -128,7 +128,9 @@ export class RewardStore {
 
     //Adds the top chops category as the first category so it will always be the first row
     categories.set('top chops', new Set())
+    categories.set('winter greens', new Set())
     categories.set('fresh loot friday', new Set())
+    categories.set('other gift cards', new Set())
     categories.set('games', new Set())
     categories.set('gaming gift cards', new Set())
     categories.set('hardware', new Set())
