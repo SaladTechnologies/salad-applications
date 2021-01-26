bfea4fb3b160bc5ba700320a674646bd39347faf
Scott Cinatl <scottcinatl@gmail.com>
2021-01-26 10:42:55 -0800

    Updated Fallback Error Modal

--- packages/web-app/src/modules/error-views/FallbackErrorContainer.ts
+++ packages/web-app/src/modules/error-views/FallbackErrorContainer.ts
@@ -1,9 +1,37 @@
 import { connect } from '../../connect'
 import { RootStore } from '../../Store'
+import { StartReason } from '../salad-bowl/models'
 import { FallbackErrorPage } from './components/FallbackErrorPage'
 
-const mapStoreToProps = (store: RootStore): any => ({
-  onCloseClicked: () => store.ui.hideModal(),
-})
+const mapStoreToProps = (store: RootStore): any => {
+  const gpuMiningEnabled = store.saladBowl.gpuMiningEnabled
+
+  let currentMinerType: string
+  let alternativeMinerType: string
+  if (gpuMiningEnabled) {
+    currentMinerType = 'GPU'
+    alternativeMinerType = 'CPU'
+  } else {
+    currentMinerType = 'CPU'
+    alternativeMinerType = 'GPU'
+  }
+
+  const onSwitchMiningType = () => {
+    const currentPath = window && window.location.pathname
+    store.analytics.trackButtonClicked(currentPath, 'switch_mining_type_button', 'Switch Mining Type Button', 'enabled')
+    store.saladBowl.setGpuOnly(!gpuMiningEnabled)
+    store.ui.hideModal()
+    store.saladBowl.start(StartReason.Manual)
+  }
+
+  return {
+    alternativeMinerType,
+    currentMinerType,
+    onCloseClicked: () => store.ui.hideModal(),
+    onOpenSupportTicket: () => store.zendesk.openSupportTicket(),
+    onSwitchMiningType,
+    gpuMiningEnabled,
+  }
+}
 
 export const FallbackErrorContainer = connect(mapStoreToProps, FallbackErrorPage)
