7d58970a53298db6e9f0f1deafa793cd6d1ea708
Scott Cinatl <scottcinatl@gmail.com>
2021-01-26 16:59:57 -0800

    Add compatibility override logic to banner Start Button

--- packages/web-app/src/modules/machine-views/StartButtonContainer.ts
+++ packages/web-app/src/modules/machine-views/StartButtonContainer.ts
@@ -1,14 +1,31 @@
 import { connect } from '../../connect'
 import { RootStore } from '../../Store'
+import { MiningStatus } from '../machine/models'
 import { StartButton } from './components/StartButton'
 
-const mapStoreToProps = (store: RootStore): any => ({
-  onClick: store.saladBowl.toggleRunning,
-  status: store.saladBowl.status,
-  isEnabled: !store.auth.isAuthenticated || store.saladBowl.canRun,
-  runningTime: store.saladBowl.runningTime,
-  pluginCount: store.saladBowl.pluginCount,
-  isDesktop: store.native.isNative,
-})
+const mapStoreToProps = (store: RootStore): any => {
+  const notCompatible = !store.saladBowl.canRun
+  const status = store.saladBowl.status
+  const isRunning =
+    status === MiningStatus.Installing || status === MiningStatus.Initializing || status === MiningStatus.Running
+  const onClick = () => {
+    const currentPath = window && window.location.pathname
+    store.analytics.trackButtonClicked(currentPath, 'start_button', 'Start Button', 'enabled')
+    if (notCompatible && !isRunning) {
+      store.ui.showModal('/errors/not-compatible')
+    } else {
+      store.saladBowl.toggleRunning()
+    }
+  }
+  return {
+    onClick,
+    isRunning,
+    status,
+    notCompatible,
+    runningTime: store.saladBowl.runningTime,
+    pluginCount: store.saladBowl.pluginCount,
+    isDesktop: store.native.isNative,
+  }
+}
 
 export const StartButtonContainer = connect(mapStoreToProps, StartButton)
