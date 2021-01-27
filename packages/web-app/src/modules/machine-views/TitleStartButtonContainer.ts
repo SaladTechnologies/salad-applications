a293425616959d163987ee1013ad2083449f5a51
Scott Cinatl <scottcinatl@gmail.com>
2021-01-26 16:23:19 -0800

    Fixes Title Start Button logic

--- packages/web-app/src/modules/machine-views/TitleStartButtonContainer.ts
+++ packages/web-app/src/modules/machine-views/TitleStartButtonContainer.ts
@@ -4,30 +4,22 @@ import { MiningStatus } from '../machine/models'
 import { TitleStartButton } from './components/TitleStartButton'
 
 const mapStoreToProps = (store: RootStore): any => {
-  const notCompatible = store.auth.isAuthenticated && !store.saladBowl.canRun
-  const isDisabled = !store.machine.cpuCompatible
+  const notCompatible = !store.saladBowl.canRun
   const status = store.saladBowl.status
   const isRunning =
     status === MiningStatus.Installing || status === MiningStatus.Initializing || status === MiningStatus.Running
 
   const onClick = () => {
     const currentPath = window && window.location.pathname
-    if (isDisabled || (notCompatible && !isRunning)) {
-      store.analytics.trackButtonClicked(
-        currentPath,
-        'start_button',
-        'Start Button',
-        isDisabled ? 'disabled' : 'enabled',
-      )
+    store.analytics.trackButtonClicked(currentPath, 'start_button', 'Start Button', 'enabled')
+    if (notCompatible && !isRunning) {
       store.ui.showModal('/errors/not-compatible')
     } else {
-      store.analytics.trackButtonClicked(currentPath, 'start_button', 'Start Button', 'enabled')
       store.saladBowl.toggleRunning()
     }
   }
 
   return {
-    isDisabled,
     isRunning,
     notCompatible,
     onClick,
