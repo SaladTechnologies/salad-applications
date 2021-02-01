b2b44622ed15982de93598c72cad44c0829c8d38
Kyle Dodson <kyledodson@gmail.com>
2021-02-01 15:41:42 -0800

    Update PhoenixMiner and GMiner and upgrade for Grin hardfork

--- packages/web-app/src/modules/salad-bowl/definitions/phoenix-miner.tsx
+++ packages/web-app/src/modules/salad-bowl/definitions/phoenix-miner.tsx
@@ -2,6 +2,11 @@ const baseUrl = 'https://github.com/SaladTechnologies/plugin-downloads/releases/
 
 export const downloads = [
   {
+    version: '5.5c',
+    linuxUrl: baseUrl + '/phoenixminer-5-5c/phoenixminer-5-5c-linux.tar.gz',
+    windowsUrl: baseUrl + '/phoenixminer-5-5c/phoenixminer-5-5c-windows.zip',
+  },
+  {
     version: '5.4c',
     linuxUrl: baseUrl + '/phoenixminer-5-4c/phoenixminer-5-4c-linux.tar.gz',
     windowsUrl: baseUrl + '/phoenixminer-5-4c/phoenixminer-5-4c-windows.zip',
@@ -10,10 +15,5 @@ export const downloads = [
     version: '5.3b',
     linuxUrl: baseUrl + '/phoenixminer-5-3b/phoenixminer-5-3b-linux.tar.gz',
     windowsUrl: baseUrl + '/phoenixminer-5-3b/phoenixminer-5-3b-windows.zip',
-  },
-  {
-    version: '5.1c',
-    linuxUrl: baseUrl + '/phoenixminer-5-1-c/phoenixminer-5-1-c-linux.tar.gz',
-    windowsUrl: baseUrl + '/phoenixminer-5-1-c/phoenixminer-5-1-c-windows.zip',
   },
 ]
