61964832cdaf975785faeb5e49955a3dab760822
Scott Cinatl <scottcinatl@gmail.com>
2021-03-11 10:30:07 -0800

    Remove Time to Chop hero and other unused heros

--- packages/web-app/src/modules/reward-views/heroes/ReferralEntryHero.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta } from '@storybook/react'
-import { ReferralEntryHero } from '.'
-
-export default {
-  title: 'Modules/Heroes/Referral Entry Hero',
-  component: ReferralEntryHero,
-} as Meta
-
-export const Default = () => <ReferralEntryHero />
