2c4dc35eb56f54f871e77149f6cf6a476ed6ad85
Scott Cinatl <scottcinatl@gmail.com>
2021-03-15 16:55:36 -0700

    Added more error messages to identify AV issue (#650)

--- packages/web-app/src/modules/salad-bowl/definitions/errors.ts
+++ packages/web-app/src/modules/salad-bowl/definitions/errors.ts
@@ -1,24 +1,36 @@
 import { ErrorAction } from '../models/ErrorAction'
 import { ErrorCategory } from '../models/ErrorCategory'
 
-export const STANDARD_ERRORS = [
-  // Anti-Virus
-  {
-    message: 'not recognized as an internal or external command',
-    errorCode: 100000000,
-    errorCategory: ErrorCategory.AntiVirus,
-    errorAction: ErrorAction.Ignore,
-  },
-  {
-    message: 'cannot find the path specified',
-    errorCode: 100000002,
-    errorCategory: ErrorCategory.AntiVirus,
-    errorAction: ErrorAction.Ignore,
-  },
-  {
-    message: 'cannot access the file because it is being used',
-    errorCode: 100000004,
-    errorCategory: ErrorCategory.AntiVirus,
-    errorAction: ErrorAction.Ignore,
-  },
+let ErrorCode = 100000000
+const antiVirusErrorMessages: string[] = [
+  'not recognized as an internal or external command',
+  'cannot find the path specified',
+  'cannot access the file because it is being used',
+  'Access is denied',
+  'Acesso negado',
+  'Accesso negato',
+  'Toegang geweigerd',
+  'Zugriff verweigert',
+  'The system cannot execute the specified program',
+  'The system cannot find the file',
+  'Command is either misspelt or could not be found',
+  'Der Prozess kann nicht auf die Datei zugreifen, da sie von einem anderen Prozess verwendet wird',
+  'The process cannot access the file because it is being used by another process',
+  'Anti-hacking system detected modification of the miner memory',
+  'riconosciuto come comando interno o esterno',
+  'Das angegebene Programm kann nicht ausgef',
+  'nds av en annan process',
+  'reconhecido como um comando interno ou externo, um programa oper',
 ]
+
+export const STANDARD_ERRORS = antiVirusErrorMessages.map((message, index) => {
+  if (index !== 0) {
+    ErrorCode += 2
+  }
+  return {
+    message: message,
+    errorCode: ErrorCode,
+    errorCategory: ErrorCategory.AntiVirus,
+    errorAction: ErrorAction.Ignore,
+  }
+})
