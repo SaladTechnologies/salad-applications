affd1e9ae504fd2eb964fe27cd394bbdf5a0871e
Jason Park <jason.j.park79@gmail.com>
2021-03-26 17:54:09 -0400

    linter

--- packages/web-app/src/modules/earn-views/ShowFolderContainer.ts
+++ packages/web-app/src/modules/earn-views/ShowFolderContainer.ts
@@ -3,7 +3,7 @@ import { RootStore } from '../../Store'
 import { ShowFolderSection } from './components/ShowFolderSection'
 
 const mapStoreToProps = (store: RootStore): any => ({
-  openFolder: store.native.openFolderLog
+  openFolder: store.native.openFolderLog,
 })
 
 export const ShowFolderContainer = connect(mapStoreToProps, ShowFolderSection)
