cf6714e7c50bdeb9ed3ffbad2b1328240e1bc3da
Jason Park <jason.j.park79@gmail.com>
2021-06-11 08:38:02 -0400

    updated to Element

--- packages/web-app/src/modules/reward-views/components/RewardDescriptionPanel.tsx
+++ packages/web-app/src/modules/reward-views/components/RewardDescriptionPanel.tsx
@@ -1,4 +1,4 @@
-// import type { ElementType } from 'htmlparser2'
+// import type { Element } from 'htmlparser2'
 import { Component, ReactElement } from 'react'
 import ReactHtmlParser from 'react-html-parser'
 import withStyles, { WithStyles } from 'react-jss'
