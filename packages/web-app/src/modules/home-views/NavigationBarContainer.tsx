3d036e5f4e59b02a259623d1ae1fd247e1e03e8b
Scott Cinatl <scottcinatl@gmail.com>
2021-06-15 16:26:14 -0700

    Connected new Navigation Bar from component library

---
+++ packages/web-app/src/modules/home-views/NavigationBarContainer.tsx
@@ -0,0 +1,44 @@
+import { AvatarDefault, NavigationBar } from '@saladtechnologies/garden-components'
+import { connect } from '../../connect'
+import { RootStore } from '../../Store'
+import { ErrorPageType } from '../../UIStore'
+import { MiningStatus } from '../machine/models'
+import { StartActionType } from '../salad-bowl/models'
+
+const mapStoreToProps = (store: RootStore): any => {
+  const isAuthenticated = store.auth.isAuthenticated
+  const handleLogin = () => {
+    store.analytics.trackButtonClicked('login_button', 'Log In Button', 'enabled')
+    store.auth.login()
+  }
+
+  const status = store.saladBowl.status
+  const isRunning = store.saladBowl.isRunning
+  const isPrepping = status !== MiningStatus.Running && store.saladBowl.runningTime !== undefined
+  const label = !store.saladBowl.isRunning
+    ? 'Start'
+    : isPrepping
+    ? status === MiningStatus.Installing
+      ? MiningStatus.Installing
+      : MiningStatus.Initializing
+    : status
+
+  return {
+    avatar: isAuthenticated ? <AvatarDefault /> : undefined, // Will Pull correct after profile page is merged in
+    balance: isAuthenticated ? store.balance.currentBalance : undefined,
+    rightSideButtonLabel: isAuthenticated ? undefined : 'Login',
+    rightSideButtonClick: isAuthenticated ? undefined : handleLogin,
+    startButtonLabel: isAuthenticated ? label : 'Login',
+    startButtonClick: isAuthenticated ? () => store.saladBowl.toggleRunning(StartActionType.StartButton) : handleLogin,
+    startButtonHoverLabel: isAuthenticated && isRunning ? 'Stop' : undefined,
+    startButtonErrorClick:
+      isAuthenticated && store.saladBowl.isNotCompatible
+        ? () => store.ui.showErrorPage(ErrorPageType.NotCompatible)
+        : undefined,
+    startButtonProgress: isPrepping ? store.saladBowl.preppingProgress : isRunning ? 1 : undefined,
+    startButtonRunningTime: store.saladBowl.runningTimeDisplay,
+    username: isAuthenticated ? store.profile.currentProfile?.username : undefined,
+  }
+}
+
+export const NavigationBarContainer = connect(mapStoreToProps, NavigationBar)
