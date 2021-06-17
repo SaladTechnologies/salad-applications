9871229556aa58ee4dc9994660b1cb6eddede5f0
jpark799 <37646831+jpark799@users.noreply.github.com>
2021-06-18 00:38:52 +0900

    profile page styling (#696)

--- packages/web-app/src/modules/account-views/account-views/components/Account.tsx
+++ packages/web-app/src/modules/account-views/account-views/components/Account.tsx
@@ -1,17 +1,37 @@
 import { Layout, Text, TextField } from '@saladtechnologies/garden-components'
 import { Component } from 'react'
 import withStyles, { WithStyles } from 'react-jss'
-import { Divider, Head } from '../../../../components'
+import { Head } from '../../../../components'
+import { SaladTheme } from '../../../../SaladTheme'
 import { withLogin } from '../../../auth-views'
 import { Avatar, Profile } from '../../../profile/models'
 import { AvatarSelect } from './AvatarSelect'
 
-const styles = {
+const styles = (theme: SaladTheme) => ({
   container: {
     flex: 1,
     backgroundImage: 'linear-gradient(to right, #56A431 , #AACF40)',
   },
-}
+  textContainer: {
+    color: theme.darkBlue,
+    paddingBottom: '25px',
+  },
+  textFieldContainer: {
+    color: theme.darkBlue,
+    width: '400px',
+    paddingBottom: '36px',
+  },
+  minecraftInfoContainer: {
+    color: theme.darkBlue,
+    width: '400px',
+  },
+  avatarContainer: {
+    paddingBottom: '58px',
+  },
+  titleContainer: {
+    paddingBottom: '35px',
+  },
+})
 
 interface Props extends WithStyles<typeof styles> {
   avatarError?: {
@@ -48,18 +68,27 @@ class _Account extends Component<Props> {
 
     return (
       <div className={classes.container}>
-        <Layout title="Profile">
+        <Layout>
+          <div className={classes.titleContainer}>
+            <Text variant="baseXXL"> Profile </Text>
+          </div>
           <Head title="Profile" />
-          <TextField
-            placeholder={profile?.username}
-            errorMessage="Username must be between 2 - 32 characters and can not contain spaces!"
-            label="Username"
-            onSubmit={onUpdateUsername}
-            validationRegex={/^\w{2,32}$/}
-          />
-          <Divider />
+          <div className={classes.textFieldContainer}>
+            <TextField
+              placeholder={profile?.username}
+              errorMessage="Username must be between 2 - 32 characters and can not contain spaces!"
+              label="Username"
+              onSubmit={onUpdateUsername}
+              validationRegex={/^\w{2,32}$/}
+              width={396}
+            />
+          </div>
+
           {avatars && (
-            <>
+            <div className={classes.avatarContainer}>
+              <div className={classes.textContainer}>
+                <Text variant="baseM"> Avatar </Text>
+              </div>
               <AvatarSelect
                 avatars={avatars}
                 error={avatarError}
@@ -68,23 +97,27 @@ class _Account extends Component<Props> {
                 onSelectAvatar={onSelectAvatar}
                 selectedAvatar={selectedAvatar}
               />
-              <Divider />
-            </>
+            </div>
           )}
 
-          <Text as="h1"> Extras</Text>
-          <TextField
-            placeholder={profile?.extensions?.minecraftUsername}
-            errorMessage="Not a valid Minecraft username!"
-            label="Minecraft Username"
-            onSubmit={onUpdateMinecraftUsername}
-            validationRegex={/^\w{3,16}$/}
-          />
-          <Text as="p">
-            Connect Salad to your Minecraft account. A Minecraft username is required to purchase many Minecraft items
-          </Text>
-
-          <Head title="Account" />
+          <div className={classes.textContainer}>
+            <Text variant="baseXXL"> Extras</Text>
+          </div>
+          <div className={classes.textFieldContainer}>
+            <TextField
+              placeholder={profile?.extensions?.minecraftUsername}
+              errorMessage="Not a valid Minecraft username!"
+              label="Minecraft Username"
+              onSubmit={onUpdateMinecraftUsername}
+              validationRegex={/^\w{3,16}$/}
+              width={396}
+            />
+          </div>
+          <div className={classes.minecraftInfoContainer}>
+            <Text variant="baseS">
+              Connect Salad to your Minecraft account. A Minecraft username is required to purchase many Minecraft items
+            </Text>
+          </div>
         </Layout>
       </div>
     )
