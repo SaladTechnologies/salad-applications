5272de460c83cf38ac5db66088b8d881ba0260f2
Jason Park <jason.j.park79@gmail.com>
2021-07-15 17:50:25 -0400

    resolved es-lint regex issue

--- packages/web-app/src/modules/auth-views/components/LoginPage.tsx
+++ packages/web-app/src/modules/auth-views/components/LoginPage.tsx
@@ -146,6 +146,8 @@ export const LoginPage = withStyles(styles)(
                         isSubmitSuccess={false}
                         onFocus={this.handleResetSubmitSuccess}
                         validationRegex={
+                          // the regex pattern below is the best pattern for recognizing emails(http://emailregex.com/) however, eslint's default settings flags control characters. We know for certain we want to use these control characters so we are ignoring this rule. (https://stackoverflow.com/questions/49743842/javascript-unexpected-control-characters-in-regular-expression)
+                          // eslint-disable-next-line no-control-regex
                           /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
                         }
                         leadingIcon={
