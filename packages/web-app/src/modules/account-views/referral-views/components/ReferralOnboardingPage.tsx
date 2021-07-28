e34d9e82f546cc3ca4c05b98b3cd266db6d7365b
Jason Park <jason.j.park79@gmail.com>
2021-07-27 21:40:07 -0400

    not showing navbar

--- packages/web-app/src/modules/account-views/referral-views/components/ReferralOnboardingPage.tsx
+++ packages/web-app/src/modules/account-views/referral-views/components/ReferralOnboardingPage.tsx
@@ -5,6 +5,7 @@ import classnames from 'classnames'
 import { Component } from 'react'
 import withStyles, { WithStyles } from 'react-jss'
 import MediaQuery from 'react-responsive'
+import { ModalPage } from '../../../../components'
 import { SaladTheme } from '../../../../SaladTheme'
 import LoginPageRewards from '../../../auth-views/assets/login-screen-rewards.png'
 import ReferralsImageStatic from '../../../auth-views/assets/referrals-image-static.png'
@@ -86,79 +87,81 @@ class _ReferralOnboardingPage extends Component<Props> {
     } = this.props
 
     return (
-      <div className={classes.page}>
-        <div className={classes.contentContainer}>
-          {currentStep === ReferralStep.Initial && (
-            <>
-              {console.log(username)}
-              <div className={classes.content}>
-                <div className={classnames(classes.header, classes.mb48)}>
-                  <Text variant="headline">Welcome to the Kitchen, {username} !</Text>
+      <ModalPage>
+        <div className={classes.page}>
+          <div className={classes.contentContainer}>
+            {currentStep === ReferralStep.Initial && (
+              <>
+                {console.log(username)}
+                <div className={classes.content}>
+                  <div className={classnames(classes.header, classes.mb48)}>
+                    <Text variant="headline">Welcome to the Kitchen, {username} !</Text>
+                  </div>
+                  <FieldContainer>
+                    <div className={classes.mb48}>
+                      <Text variant="baseL">
+                        You’ve joined a community of over 500,000 Chefs who use Salad to earn games, gift cards, and
+                        other rewards.
+                      </Text>
+                    </div>
+                    <div className={classes.mb48}>
+                      <Text variant="baseL">
+                        Now, let’s get your machine set up with a quick process to maximize your earning rates.
+                      </Text>
+                    </div>
+                  </FieldContainer>
+                  <div>
+                    <Button onClick={onNextPage} trailingIcon={<ChevronRight />} label="let's go"></Button>
+                  </div>
                 </div>
-                <FieldContainer>
-                  <div className={classes.mb48}>
-                    <Text variant="baseL">
-                      You’ve joined a community of over 500,000 Chefs who use Salad to earn games, gift cards, and other
-                      rewards.
-                    </Text>
+                <MediaQuery minWidth={767}>
+                  <img className={classes.rightSideImage} src={LoginPageRewards} alt="Salad Reward Items" />
+                </MediaQuery>
+              </>
+            )}
+            {currentStep === ReferralStep.Code && (
+              <>
+                <div className={classes.content}>
+                  <div className={classnames(classes.header, classes.mb48)}>
+                    <Text variant="headline">Enter your Referral Code</Text>
                   </div>
-                  <div className={classes.mb48}>
-                    <Text variant="baseL">
-                      Now, let’s get your machine set up with a quick process to maximize your earning rates.
-                    </Text>
+                  <FieldContainer>
+                    <div className={classes.mb48}>
+                      <Text variant="baseL">
+                        If you received a promo code, enter it below. This boosts your earning rate and lets us give
+                        credit to your referrer, too.
+                      </Text>
+                    </div>
+                    <div className={classes.mb48}>
+                      <TextField
+                        label="Promo Code"
+                        validationRegex={/^.{1,10}/}
+                        validationRegexErrorMessage={'Invalid Code. Codes are less than 10 characters with no spaces.'}
+                        onSubmit={this.handleSubmitCode}
+                        isSubmitting={isSubmittingReferralCode}
+                        isSubmitSuccess={isReferralCodeSubmitSuccess}
+                        serverSideErrorMessage={serverSideErrorMessage}
+                      />
+                    </div>
+                    <div className={classes.mb48}>
+                      <Text variant="baseL">
+                        Didn’t receive a promo code? No problem! We’ll enter one automatically so you can still get a
+                        bonus.
+                      </Text>
+                    </div>
+                  </FieldContainer>
+                  <div>
+                    <Button onClick={this.handleDefaultCode} variant="outlined" label="Give me a bonus!" />
                   </div>
-                </FieldContainer>
-                <div>
-                  <Button onClick={onNextPage} trailingIcon={<ChevronRight />} label="let's go"></Button>
                 </div>
-              </div>
-              <MediaQuery minWidth={767}>
-                <img className={classes.rightSideImage} src={LoginPageRewards} alt="Salad Reward Items" />
-              </MediaQuery>
-            </>
-          )}
-          {currentStep === ReferralStep.Code && (
-            <>
-              <div className={classes.content}>
-                <div className={classnames(classes.header, classes.mb48)}>
-                  <Text variant="headline">Enter your Referral Code</Text>
-                </div>
-                <FieldContainer>
-                  <div className={classes.mb48}>
-                    <Text variant="baseL">
-                      If you received a promo code, enter it below. This boosts your earning rate and lets us give
-                      credit to your referrer, too.
-                    </Text>
-                  </div>
-                  <div className={classes.mb48}>
-                    <TextField
-                      label="Promo Code"
-                      validationRegex={/^.{1,10}/}
-                      validationRegexErrorMessage={'Invalid Code. Codes are less than 10 characters with no spaces.'}
-                      onSubmit={this.handleSubmitCode}
-                      isSubmitting={isSubmittingReferralCode}
-                      isSubmitSuccess={isReferralCodeSubmitSuccess}
-                      serverSideErrorMessage={serverSideErrorMessage}
-                    />
-                  </div>
-                  <div className={classes.mb48}>
-                    <Text variant="baseL">
-                      Didn’t receive a promo code? No problem! We’ll enter one automatically so you can still get a
-                      bonus.
-                    </Text>
-                  </div>
-                </FieldContainer>
-                <div>
-                  <Button onClick={this.handleDefaultCode} variant="outlined" label="Give me a bonus!" />
-                </div>
-              </div>
-              <MediaQuery minWidth={767}>
-                <img className={classes.rightSideImage} src={ReferralsImageStatic} alt="Salad Reward Items" />
-              </MediaQuery>
-            </>
-          )}
+                <MediaQuery minWidth={767}>
+                  <img className={classes.rightSideImage} src={ReferralsImageStatic} alt="Salad Reward Items" />
+                </MediaQuery>
+              </>
+            )}
+          </div>
         </div>
-      </div>
+      </ModalPage>
     )
   }
 }
