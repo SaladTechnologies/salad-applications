6a7f7f2da597b3a4ade70e019e3e9201e063e0ed
Jason Park <jason.j.park79@gmail.com>
2021-08-23 16:08:09 -0400

    showing notification

--- packages/web-app/src/modules/reward/RewardStore.ts
+++ packages/web-app/src/modules/reward/RewardStore.ts
@@ -289,6 +289,16 @@ export class RewardStore {
                 onClick: () => this.store.routing.push('/settings/summary'),
                 type: 'error',
               }
+            } else if (response.data?.type === 'redemptions:dailySpendLimitExceeded') {
+              notification = {
+                category: NotificationMessageCategory.Error,
+                title: 'Daily redemption limit has been reached.',
+                message:
+                  "Sorry, Chef! It looks like you've reached your daily daily redemption limit. Click here to learn more about daily limits, and come back tomorrow.",
+                autoClose: false,
+                onClick: () => window.open('https://support.salad.com/hc/en-us/articles/4405644006932', '_blank'),
+                type: 'error',
+              }
             }
             break
         }
@@ -312,6 +322,11 @@ export class RewardStore {
       console.error('Cleared isRedeeming flag')
     }
   })
+
+  @action
+  newLink() {
+    window.open('https://support.salad.com/hc/en-us/articles/4405644006932', '_blank')
+  }
 
   @action
   clearRedemptionInfo() {
