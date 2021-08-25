b4ebc9edf0db62844431e33de8cdbaaecd551845
Jason Park <jason.j.park79@gmail.com>
2021-08-25 14:43:05 -0400

    updated ternary in whitelist-windows-defender

--- packages/desktop-app/src/main.ts
+++ packages/desktop-app/src/main.ts
@@ -387,61 +387,38 @@ const createMainWindow = () => {
   })
 
   bridge.on('whitelist-windows-defender', (filePath?: string) => {
-    filePath
-      ? exec(
-          powershellWhitelistCommand,
-          {
-            env: {
-              WHITELIST_DIR: filePath,
-            },
-            timeout: 60000,
-            windowsHide: true,
+    if (filePath || process.env.APPDATA)
+      exec(
+        powershellWhitelistCommand,
+        {
+          env: {
+            WHITELIST_DIR: filePath ? filePath : path.join(process.env.APPDATA!, 'Salad/plugin-bin'),
           },
-          (error, _stdout, stderr) => {
-            if (error) {
-              console.error(
-                `Failed to Whitelist Windows Defender (${error.message}${
-                  error.code == null ? '' : `, exit code ${error.code}`
-                })${stderr ? `\n${stderr}` : ''}`,
-              )
+          timeout: 60000,
+          windowsHide: true,
+        },
+        (error, _stdout, stderr) => {
+          if (error) {
+            console.error(
+              `Failed to Whitelist Windows Defender (${error.message}${
+                error.code == null ? '' : `, exit code ${error.code}`
+              })${stderr ? `\n${stderr}` : ''}`,
+            )
 
-              bridge.send('whitelist-windows-defender', {
-                errorCode: error.code,
-              })
-            } else {
-              bridge.send('whitelist-windows-defender', { errorCode: undefined })
-            }
-          },
-        )
-      : process.env.APPDATA
-      ? exec(
-          powershellWhitelistCommand,
-          {
-            env: {
-              WHITELIST_DIR: path.join(process.env.APPDATA, 'Salad/plugin-bin'),
-            },
-            timeout: 60000,
-            windowsHide: true,
-          },
-          (error, _stdout, stderr) => {
-            if (error) {
-              console.error(
-                `Failed to Whitelist Windows Defender (${error.message}${
-                  error.code == null ? '' : `, exit code ${error.code}`
-                })${stderr ? `\n${stderr}` : ''}`,
-              )
-
-              bridge.send('whitelist-windows-defender', {
-                errorCode: error.code,
-              })
-            } else {
-              bridge.send('whitelist-windows-defender', { errorCode: undefined })
-            }
-          },
-        )
-      : console.error(
-          `Failed to Whitelist Windows Defender because the user's APPDATA environment variable has not been defined`,
-        )
+            bridge.send('whitelist-windows-defender', {
+              errorCode: error.code,
+            })
+          } else {
+            bridge.send('whitelist-windows-defender', { errorCode: undefined })
+          }
+        },
+      )
+    else {
+      console.error(
+        `Failed to Whitelist Windows Defender because the user's APPDATA environment variable has not been defined`,
+      )
+      bridge.send('whitelist-windows-defender', { errorCode: 1 })
+    }
   })
 
   bridge.on('minimize-window', () => {
