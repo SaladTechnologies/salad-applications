4b55fc9c04297eac572cc2c392f2bd2841925e89
Jason Park <jason.j.park79@gmail.com>
2021-08-27 16:07:29 -0400

    added mixpanel tracking

--- packages/web-app/src/modules/onboarding-views/AutoStartConfigurationContainer.tsx
+++ packages/web-app/src/modules/onboarding-views/AutoStartConfigurationContainer.tsx
@@ -3,9 +3,19 @@ import { RootStore } from '../../Store'
 import { AutoStartConfigurationPage } from './pages/AutoStartConfigurationPage'
 
 const mapStoreToProps = (store: RootStore): any => {
+  const handleEnableAutoStart = () => {
+    store.analytics.trackButtonClicked('enable_auto_start_button', 'Enable Auto-Start Button', 'enabled')
+    store.onboarding.enableAutoStart()
+  }
+
+  const handleOnSkipAutoStart = () => {
+    store.analytics.trackButtonClicked('skip_auto_start_button', 'Skip Auto-Start Button', 'enabled')
+    store.onboarding.skipAutoStart()
+  }
+
   return {
-    onEnableAutoStart: store.onboarding.enableAutoStart,
-    onSkipAutoStart: store.onboarding.skipAutoStart,
+    onEnableAutoStart: handleEnableAutoStart,
+    onSkipAutoStart: handleOnSkipAutoStart,
     disableAutoStartErrorMessage: store.onboarding.disableAutoStartErrorMessage,
     disableAutoStartPending: store.onboarding.disableAutoStartPending,
     isNative: store.native.isNative,
--- packages/web-app/src/modules/onboarding-views/SleepModeConfigurationContainer.tsx
+++ packages/web-app/src/modules/onboarding-views/SleepModeConfigurationContainer.tsx
@@ -3,9 +3,19 @@ import { RootStore } from '../../Store'
 import { SleepModeConfigurationPage } from './pages/SleepModeConfigurationPage'
 
 const mapStoreToProps = (store: RootStore): any => {
+  const handleDisableSleepMode = () => {
+    store.analytics.trackButtonClicked('disable_sleep_mode_button', 'Disable Sleep-Mode Button', 'enabled')
+    store.onboarding.disableSleepMode()
+  }
+
+  const handleSkipSleepMode = () => {
+    store.analytics.trackButtonClicked('skip_sleep_mode_button', 'Skip Sleep-Mode Button', 'enabled')
+    store.onboarding.skipSleepMode()
+  }
+
   return {
-    onDisableSleepMode: store.onboarding.disableSleepMode,
-    onSkipSleepMode: store.onboarding.skipSleepMode,
+    onDisableSleepMode: handleDisableSleepMode,
+    onSkipSleepMode: handleSkipSleepMode,
     disableSleepModeErrorMessage: store.onboarding.disableSleepModeErrorMessage,
     disableSleepModePending: store.onboarding.disableSleepModePending,
     isNative: store.native.isNative,
