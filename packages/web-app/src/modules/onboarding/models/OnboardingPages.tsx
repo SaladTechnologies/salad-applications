d7c67f3c1f93d548d6548b081b1db7a25871cc2c
Jason Park <jason.j.park79@gmail.com>
2021-08-27 22:05:31 -0400

    added back afk config to enum

--- packages/web-app/src/modules/onboarding/models/OnboardingPages.tsx
+++ packages/web-app/src/modules/onboarding/models/OnboardingPages.tsx
@@ -1,6 +1,7 @@
 export enum ONBOARDING_PAGE_NAMES {
   WELCOME = 'Onboarding Welcome',
   REFERRAL = 'Onboarding Enter Referral Code',
+  AFK_CONFIGURATION = 'Onboarding AFK Configuration',
   AUTO_START_CONFIGURATION = 'Onboarding Auto-Start Configuration',
   SLEEP_MODE_CONFIGURATION = 'Onboarding Sleep-Mode Configuration',
 }
