db42b58e55f7259c7e7e52d6e03ac0d14767320c
Jason Park <jason.j.park79@gmail.com>
2021-09-02 18:58:52 -0400

    general error message

--- packages/web-app/src/modules/onboarding/models/WhitelistWindowsDefenderErrorType.ts
+++ packages/web-app/src/modules/onboarding/models/WhitelistWindowsDefenderErrorType.ts
@@ -1,4 +1,4 @@
 export enum WhitelistWindowsDefenderErrorType {
   USER_SELECTED_NO = 'User Selected No',
-  GENERAL_SCRIPT_ERROR = 'General Script Error',
+  GENERAL_SCRIPT_ERROR = 'Something went wrong and we were unable to whitelist Windows Defender. You can adjust these settings yourself in Windows Security Settings, or contact support for assistance.',
 }
