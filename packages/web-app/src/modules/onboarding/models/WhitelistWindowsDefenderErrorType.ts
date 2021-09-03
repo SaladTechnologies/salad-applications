4cdaba40d02efa9d89b6898b359a92b67d702851
Jason Park <jason.j.park79@gmail.com>
2021-09-03 17:09:39 -0400

    updated error message

--- packages/web-app/src/modules/onboarding/models/WhitelistWindowsDefenderErrorType.ts
+++ packages/web-app/src/modules/onboarding/models/WhitelistWindowsDefenderErrorType.ts
@@ -1,4 +1,4 @@
 export enum WhitelistWindowsDefenderErrorType {
-  USER_SELECTED_NO = `You’ll need to click ‘Yes’ on the Microsoft Access Control screen to whitelist. Please try again.`,
+  USER_SELECTED_NO = `You need to click 'Yes' on the User Account Control Prompt to whitelist Windows Defender. Please try again.`,
   GENERAL_SCRIPT_ERROR = `Sorry, something went wrong. Try following the guide below to whitelist manually, or contact support for assistance.`,
 }
