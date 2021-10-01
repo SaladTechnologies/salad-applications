e28a3ec7ffe43ed63905d9e9f07493647b94fd0c
Jason Park <jason.j.park79@gmail.com>
2021-10-01 15:24:17 -0400

    fixed sleepmode buttons

--- packages/web-app/src/modules/onboarding-views/pages/SleepModeConfigurationPage.tsx
+++ packages/web-app/src/modules/onboarding-views/pages/SleepModeConfigurationPage.tsx
@@ -63,11 +63,7 @@ const styles = (theme: SaladTheme) => ({
     width: '100%',
   },
   enableButton: {
-    width: 180,
     marginLeft: 24,
-  },
-  skipButton: {
-    width: 145,
   },
   buttonsContainer: {
     display: 'flex',
@@ -113,7 +109,7 @@ const _SleepModeConfigurationPage = ({
               <Text variant="baseS">You can always adjust sleep mode later in Windows settings.</Text>
             </div>
             <div className={classes.buttonsContainer}>
-              <span className={classes.skipButton}>
+              <span>
                 <Button size="medium" label="Skip For Now" onClick={onSkipSleepMode} variant="outlined" />
               </span>
               <span className={classes.enableButton}>
