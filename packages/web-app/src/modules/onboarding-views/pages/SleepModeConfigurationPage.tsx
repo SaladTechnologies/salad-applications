34da82dabf4137e87616722470502fc94ebc03cb
Jason Park <jason.j.park79@gmail.com>
2021-10-05 16:05:28 -0400

    removed span

--- packages/web-app/src/modules/onboarding-views/pages/SleepModeConfigurationPage.tsx
+++ packages/web-app/src/modules/onboarding-views/pages/SleepModeConfigurationPage.tsx
@@ -109,9 +109,7 @@ const _SleepModeConfigurationPage = ({
               <Text variant="baseS">You can always adjust sleep mode later in Windows settings.</Text>
             </div>
             <div className={classes.buttonsContainer}>
-              <span>
-                <Button size="medium" label="Skip For Now" onClick={onSkipSleepMode} variant="outlined" />
-              </span>
+              <Button size="medium" label="Skip For Now" onClick={onSkipSleepMode} variant="outlined" />
               <span className={classes.enableButton}>
                 <Button
                   errorMessage={sleepModeErrorMessage}
