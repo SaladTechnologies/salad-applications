7c9768334abf3ade407eb02169559a6d1333c070
Kyle Dodson <kyledodson@gmail.com>
2021-10-07 22:02:02 -0700

    Add login & logout support to the Help Scout support widget

--- packages/web-app/src/modules/zendesk/models/ZendeskWebWidget.ts
+++ packages/web-app/src/modules/zendesk/models/ZendeskWebWidget.ts
@@ -1,5 +1,21 @@
 import type { Ref } from 'react'
 
+declare function Beacon(method: 'init', beaconId: string): void
+declare function Beacon(method: 'destroy'): void
+declare function Beacon(method: 'open'): void
+declare function Beacon(method: 'close'): void
+declare function Beacon(method: 'toggle'): void
+declare function Beacon(method: 'search', query: string): void
+declare function Beacon(method: 'suggest'): void
+declare function Beacon(method: 'article', articleId: string, options?: { type: 'modal' | 'sidebar' }): void
+declare function Beacon(method: 'navigate', route: string): void
+declare function Beacon(method: 'identify', userObject: { name: string; email: string; signature?: string }): void
+declare function Beacon(method: 'prefill', formObject: {}): void
+declare function Beacon(method: 'reset'): void
+declare function Beacon(method: 'logout', options?: { endActiveChat: true }): void
+declare function Beacon(method: 'config', formObject: {}): void
+declare function Beacon(method: 'info'): void
+
 export interface ZendeskWidget {
   (type: 'webWidget:on' | 'webWidget' | 'webWidget:get', command: string, payload?: any): void
   (type: 'webWidget', command: 'updateSettings', payload: ZendeskSettings): void
@@ -151,6 +167,7 @@ export interface ZendeskSettings {
 
 declare global {
   interface Window {
+    Beacon?: typeof Beacon
     zESettings?: ZendeskSettings
     zE?: ZendeskWidget
     // https://support.trustpilot.com/hc/en-us/articles/115011421468--Add-a-TrustBox-to-a-single-page-application
