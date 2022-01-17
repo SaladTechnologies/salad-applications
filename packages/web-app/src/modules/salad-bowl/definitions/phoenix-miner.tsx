30cb65abe0ab70155bd6fba6afc1467888b1d41a
Kyle Dodson <kyledodson@gmail.com>
2022-01-17 13:24:10 -0800

    Add PhoenixMiner 5.9d

--- packages/web-app/src/modules/salad-bowl/definitions/phoenix-miner.tsx
+++ packages/web-app/src/modules/salad-bowl/definitions/phoenix-miner.tsx
@@ -2,6 +2,10 @@ const baseUrl = 'https://github.com/SaladTechnologies/plugin-downloads/releases/
 
 export const downloads = [
   {
+    version: '5.9d',
+    windowsUrl: baseUrl + '/phoenixminer-5-9d/phoenixminer-5-9d-windows.zip',
+  },
+  {
     version: '5.7b',
     linuxUrl: baseUrl + '/phoenixminer-5-7b/phoenixminer-5-7b-linux.tar.gz',
     windowsUrl: baseUrl + '/phoenixminer-5-7b/phoenixminer-5-7b-windows.zip',
@@ -10,10 +14,5 @@ export const downloads = [
     version: '5.6d',
     linuxUrl: baseUrl + '/phoenixminer-5-6d/phoenixminer-5-6d-linux.tar.gz',
     windowsUrl: baseUrl + '/phoenixminer-5-6d/phoenixminer-5-6d-windows.zip',
-  },
-  {
-    version: '5.5c',
-    linuxUrl: baseUrl + '/phoenixminer-5-5c/phoenixminer-5-5c-linux.tar.gz',
-    windowsUrl: baseUrl + '/phoenixminer-5-5c/phoenixminer-5-5c-windows.zip',
   },
 ]
