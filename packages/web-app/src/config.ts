cd6ac3f9327ca56d2bc465b3e9f1d1e79131e6ba
Kyle Dodson <kyledodson@gmail.com>
2022-01-17 21:50:22 -0800

    Update npm dependencies (#857)

--- packages/web-app/src/config.ts
+++ packages/web-app/src/config.ts
@@ -36,7 +36,6 @@ const requiredString = (name: string, defaultValue?: string): string => {
 
 export class Config {
   public readonly appBuild: string = requiredString('REACT_APP_BUILD')
-  public readonly appRoutingBasename?: string = optionalString('PUBLIC_URL')
   public readonly dataRefreshRate: number = numberOrDefault('REACT_APP_DATA_REFRESH_RATE', convertMinutes(5))
   public readonly mixpanelToken?: string = optionalString('REACT_APP_MIXPANEL_TOKEN')
   public readonly rewardRefreshRate: number = numberOrDefault('REACT_APP_REWARD_REFRESH_RATE', convertMinutes(5))
