48bfe9eb71c254a9854b2f0557dc5fc3be81025f
Kyle Dodson <kyledodson@gmail.com>
2022-01-21 16:44:52 -0800

    Add Prohashing KAWPOW plugin definition

--- packages/web-app/src/modules/salad-bowl/definitions/xmrig.tsx
+++ packages/web-app/src/modules/salad-bowl/definitions/xmrig.tsx
@@ -2,23 +2,13 @@ const baseUrl = 'https://github.com/SaladTechnologies/plugin-downloads/releases/
 
 export const downloads = [
   {
+    version: '6.16.2',
+    windowsUrl: baseUrl + '/xmrig-6-16-2/xmrig-6-16-2-windows.zip',
+  },
+  {
     version: '6.12.1',
     linuxUrl: baseUrl + '/xmrig-6.12.1/xmrig-6.12.1-linux.tar.gz',
     macOSUrl: baseUrl + '/xmrig-6.12.1/xmrig-6.12.1-macos.tar.gz',
     windowsUrl: baseUrl + '/xmrig-6.12.1/xmrig-6.12.1-windows.zip',
-  },
-  {
-    version: '6.7.0',
-    linuxUrl: baseUrl + '/xmrig-6.7.0/xmrig-6.7.0-linux.tar.gz',
-    macOSUrl: baseUrl + '/xmrig-6.7.0/xmrig-6.7.0-macos.tar.gz',
-    windowsCudaUrl: baseUrl + '/xmrig-6.7.0/xmrig-6.7.0-windows-cuda.zip',
-    windowsOpenCLUrl: baseUrl + '/xmrig-6.7.0/xmrig-6.7.0-windows-opencl.zip',
-  },
-  {
-    version: '6.6.2',
-    linuxUrl: baseUrl + '/xmrig-6.6.2/xmrig-6.6.2-linux.tar.gz',
-    macOSUrl: baseUrl + '/xmrig-6.6.2/xmrig-6.6.2-macos.tar.gz',
-    windowsCudaUrl: baseUrl + '/xmrig-6.6.2/xmrig-6.6.2-windows-cuda.zip',
-    windowsOpenCLUrl: baseUrl + '/xmrig-6.6.2/xmrig-6.6.2-windows-opencl.zip',
   },
 ]
