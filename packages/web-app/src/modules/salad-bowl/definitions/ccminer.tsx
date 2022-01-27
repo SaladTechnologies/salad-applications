abeaa21dcc31ea8f041fc2076f7c5fa0c99dd0f6
Kyle Dodson <kyledodson@gmail.com>
2022-01-26 22:52:00 -0800

    Remove ccminer

--- packages/web-app/src/modules/salad-bowl/definitions/ccminer.tsx
+++
@@ -1,8 +0,0 @@
-const baseUrl = 'https://github.com/SaladTechnologies/plugin-downloads/releases/download'
-
-export const downloads = [
-  {
-    version: '2.3.1',
-    windowsUrl: baseUrl + '/ccminer-2.31/ccminer-2-3-1-windows.zip',
-  },
-]
