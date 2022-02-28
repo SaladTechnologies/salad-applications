acddbcae81ca5433a4df29172942090f3e81fcf7
MerlynTheWizard <cameron@salad.io>
2022-02-28 13:25:48 +0000

    updates Gminer to v2.82

--- packages/web-app/src/modules/salad-bowl/definitions/gminer.tsx
+++ packages/web-app/src/modules/salad-bowl/definitions/gminer.tsx
@@ -2,16 +2,16 @@ const baseUrl = 'https://github.com/SaladTechnologies/plugin-downloads/releases/
 
 export const downloads = [
   {
+    version: '2.82',
+    windowsUrl: baseUrl + '/gminer-2-82/gminer-2-82-windows.zip',
+  },
+  {
+    version: '2.79',
+    windowsUrl: baseUrl + '/gminer-2-79/gminer-2-79-windows.zip',
+  },
+  {
     version: '2.78',
     windowsUrl: baseUrl + '/gminer-2-78/gminer-2-78-windows.zip',
-  },
-  {
-    version: '2.76',
-    windowsUrl: baseUrl + '/gminer-2-76/gminer-2-76-windows.zip',
-  },
-  {
-    version: '2.75',
-    windowsUrl: baseUrl + '/gminer-2-75/gminer-2-75-windows.zip',
   },
   {
     version: '2.54',
