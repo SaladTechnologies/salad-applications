f3419660b389bbe3fa5635cea8e0b9f2f419fb60
Cameron Davidson <62080855+MerlynTheWizard@users.noreply.github.com>
2022-04-13 07:53:34 -0600

    Updates PhoenixMiner to v6.1b (#910)

--- packages/web-app/src/modules/salad-bowl/definitions/phoenix-miner.tsx
+++ packages/web-app/src/modules/salad-bowl/definitions/phoenix-miner.tsx
@@ -1,7 +1,11 @@
 const baseUrl = 'https://github.com/SaladTechnologies/plugin-downloads/releases/download'
 
 export const downloads = [
-    {
+  {
+    version: '6.1b',
+    windowsUrl: baseUrl + '/phoenixminer-6-1b/phoenixminer-6-1b-windows.zip',
+  },
+  {
     version: '6.0c',
     windowsUrl: baseUrl + '/phoenixminer-6-0c/phoenixminer-6-0c-windows.zip',
   },
@@ -13,10 +17,5 @@ export const downloads = [
     version: '5.7b',
     linuxUrl: baseUrl + '/phoenixminer-5-7b/phoenixminer-5-7b-linux.tar.gz',
     windowsUrl: baseUrl + '/phoenixminer-5-7b/phoenixminer-5-7b-windows.zip',
-  },
-  {
-    version: '5.6d',
-    linuxUrl: baseUrl + '/phoenixminer-5-6d/phoenixminer-5-6d-linux.tar.gz',
-    windowsUrl: baseUrl + '/phoenixminer-5-6d/phoenixminer-5-6d-windows.zip',
   },
 ]
