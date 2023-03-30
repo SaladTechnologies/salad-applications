29d21554cef7ae7bdac8276aac7d32b77c187a30
Ruslan Ners <ruslan.nersezashvili@salad.com>
2023-03-30 11:46:29 +0700

    refactor due to comments

---
+++ packages/web-app/src/modules/reward-views/components/RewardCard/RewardCard.tsx
@@ -0,0 +1,134 @@
+import { Button } from '@saladtechnologies/garden-components'
+import classNames from 'classnames'
+import type { FunctionComponent } from 'react'
+import { Img } from 'react-image'
+import type { WithStyles } from 'react-jss'
+import withStyles from 'react-jss'
+import Skeleton from 'react-loading-skeleton'
+import type { SaladTheme } from '../../../../SaladTheme'
+import { DefaultTheme } from '../../../../SaladTheme'
+import type { Reward } from '../../../reward/models'
+import { RewardMissingImage } from '../RewardMissingImage'
+
+const styles = (theme: SaladTheme) => ({
+  selected: {},
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: 180,
+    cursor: 'pointer',
+    ':is(&:hover, &$selected) $buttonWrapper': {
+      display: 'block',
+    },
+    ':is(&:hover, &$selected) $name': {
+      color: theme.mediumGreen,
+    },
+    ':is(&:hover) $imageWrapper': {
+      background: 'linear-gradient(0deg, rgba(83, 166, 38, 0.2), rgba(83, 166, 38, 0.2))',
+    },
+    ':is(&$selected) $imageWrapper': {
+      background: 'linear-gradient(180deg, rgba(177, 209, 53, 0.4) 0%, rgba(83, 166, 38, 0.4) 100%)',
+    },
+    ':is(&$selected) $image': {
+      border: `4px solid ${theme.mediumGreen}`,
+    },
+  },
+  imageWrapper: {
+    display: 'flex',
+    position: 'relative',
+    height: 241,
+    width: '100%',
+    marginBottom: 32,
+  },
+  image: {
+    zIndex: -1,
+    height: '100%',
+    width: '100%',
+    boxSizing: 'border-box',
+    filter: 'drop-shadow(8px 14px 21px rgba(0, 0, 0, 0.45))',
+  },
+  name: {
+    color: theme.lightGreen,
+    textAlign: 'left',
+    fontFamily: theme.fontGroteskLight09,
+    fontWeight: 300,
+    fontSize: 48,
+    lineHeight: '48px',
+    overflow: 'hidden',
+    display: '-webkit-box',
+    WebkitLineClamp: '2',
+    WebkitBoxOrient: 'vertical',
+    textOverflow: 'ellipsis',
+    marginTop: 'auto',
+    marginBottom: 4,
+  },
+  price: {
+    textAlign: 'left',
+    color: theme.green,
+    fontFamily: theme.fontGroteskBook25,
+    fontWeight: 400,
+    fontSize: 12,
+    lineHeight: '18px',
+    marginTop: 'auto',
+  },
+  buttonWrapper: {
+    display: 'none',
+    position: 'absolute',
+    bottom: -18,
+    left: 0,
+    right: 0,
+    margin: 'auto',
+    width: 143,
+  },
+})
+
+interface Props extends WithStyles<typeof styles> {
+  reward: Reward
+  isSelected: boolean
+  onSelectTargetRewardClick: (rewardId: string) => void
+  onConfirmTargetRewardSelectionClick: (reward: Reward) => void
+}
+
+const _RewardCard: FunctionComponent<Props> = ({
+  classes,
+  reward,
+  isSelected,
+  onSelectTargetRewardClick,
+  onConfirmTargetRewardSelectionClick,
+}) => {
+  const handleRewardClick = () => {
+    if (isSelected) {
+      return onConfirmTargetRewardSelectionClick(reward)
+    }
+
+    return onSelectTargetRewardClick(reward.id)
+  }
+
+  const getButton = () => {
+    if (isSelected) {
+      return <Button variant="primary" label="Confirm" />
+    }
+
+    return <Button variant="secondary" outlineColor={DefaultTheme.mediumGreen} label="Target" />
+  }
+
+  return (
+    <div onClick={handleRewardClick} className={classNames(classes.container, isSelected && classes.selected)}>
+      <div className={classes.imageWrapper}>
+        <Img
+          className={classes.image}
+          src={reward.coverImage ?? ''}
+          draggable={false}
+          alt={reward.name}
+          loader={<Skeleton height={'100%'} />}
+          unloader={<RewardMissingImage text={reward.name} />}
+        />
+        <div className={classes.buttonWrapper}>{getButton()}</div>
+      </div>
+      <div className={classes.name}>{reward.name}</div>
+      <div className={classes.price}>{reward.price.toFixed(2)}</div>
+    </div>
+  )
+}
+
+export const RewardCard = withStyles(styles)(_RewardCard)
--- packages/web-app/src/modules/reward-views/pages/SelectTargetRewardPage.tsx
+++ packages/web-app/src/modules/reward-views/pages/SelectTargetRewardPage.tsx
@@ -1,15 +1,15 @@
-import type { ReactNode } from 'react'
-import { Component } from 'react'
+import type { FunctionComponent } from 'react'
+import { useState } from 'react'
 import type { WithStyles } from 'react-jss'
 import withStyles from 'react-jss'
 import { Scrollbar } from '../../../components'
 import type { Reward } from '../../reward/models'
-import { TargetRewardItem } from '../components/TargetRewardItem'
+import { RewardCard } from '../components/RewardCard'
 import FortniteImageURL from './assets/fortnite.png'
 import NorthAmerica5URL from './assets/northAmerica5.png'
 import Xbox10ImageURL from './assets/xbox10.png'
 
-const styles = () => ({
+const styles = {
   targetRewards: {
     display: 'flex',
     margin: '5rem 1rem 0',
@@ -18,78 +18,66 @@ const styles = () => ({
     maxWidth: 655,
     flexWrap: 'wrap',
   },
-})
+}
 
 interface Props extends WithStyles<typeof styles> {
   targetRewards: Reward[]
   onSelectTargetReward: (reward: Reward) => void
 }
 
-interface State {
-  selectedTargetRewardId: string | null
-}
+const _SelectTargetRewardPage: FunctionComponent<Props> = ({ classes, targetRewards, onSelectTargetReward }) => {
+  const [selectedRewardId, setSelectedRewardId] = useState<string | null>(null)
 
-class _SelectTargetRewardPage extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      selectedTargetRewardId: null,
-    }
+  const handleSelectTargetRewardClick = (rewardId: string) => {
+    setSelectedRewardId(rewardId)
   }
 
-  onSelectTargetRewardClick = (rewardId: string) => {
-    this.setState({ selectedTargetRewardId: rewardId })
-  }
-
-  onConfirmTargetRewardSelectionClick = (targetReward: Reward) => {
+  const handleConfirmTargetRewardSelectionClick = (targetReward: Reward) => {
     // * TODO: Add redirect to the "You've selected ..." new page
-    this.props.onSelectTargetReward(targetReward)
+    onSelectTargetReward(targetReward)
   }
 
-  public override render(): ReactNode {
-    const { classes } = this.props
-
-    const mockedTargetRewards = [
+  const mockedTargetRewards =
+    [
       {
         id: 'd214ac17-daaf-4e14-87ac-036f93f32474',
-        name: 'Discord Nitro',
-        price: 10.0,
+        name: 'Fortnite Harley Quinn Back Bling DLC Some DL',
+        price: 12.79,
         tags: [],
         coverImage: FortniteImageURL,
       },
       {
         id: 'ec83ab9f-22f0-4de5-ab33-7af39289da2c',
-        name: 'Fortnite Harley Quinn Back Bling DLC Some DL',
-        price: 20.0,
+        name: 'Xbox Live - $5 Gift Card ',
+        price: 9.99,
         tags: [],
         coverImage: Xbox10ImageURL,
       },
       {
         id: '6c6d069b-5f57-4429-b7c4-4f4f174eef31',
-        name: 'Valorant - $5 Gift Card [NA]',
+        name: 'Valorant - $5 Card',
         price: 5.0,
         tags: [],
         coverImage: NorthAmerica5URL,
       },
-    ]
+    ] ?? targetRewards
 
-    return (
-      <Scrollbar>
-        <div className={classes.targetRewards}>
-          {/* * NOTE: Should be replaced with "targetRewards" from props when backend is ready */}
-          {mockedTargetRewards.map((targetReward) => (
-            <TargetRewardItem
-              key={targetReward.id}
-              isSelected={this.state.selectedTargetRewardId === targetReward.id}
-              targetReward={targetReward}
-              onSelectTargetRewardClick={this.onSelectTargetRewardClick}
-              onConfirmTargetRewardSelectionClick={this.onConfirmTargetRewardSelectionClick}
-            />
-          ))}
-        </div>
-      </Scrollbar>
-    )
-  }
+  return (
+    <Scrollbar>
+      <div className={classes.targetRewards}>
+        {/* * NOTE: Should be replaced with "targetRewards" from props when backend is ready */}
+        {mockedTargetRewards.map((targetReward) => (
+          <RewardCard
+            key={targetReward.id}
+            reward={targetReward}
+            isSelected={selectedRewardId === targetReward.id}
+            onSelectTargetRewardClick={handleSelectTargetRewardClick}
+            onConfirmTargetRewardSelectionClick={handleConfirmTargetRewardSelectionClick}
+          />
+        ))}
+      </div>
+    </Scrollbar>
+  )
 }
 
 export const SelectTargetRewardPage = withStyles(styles)(_SelectTargetRewardPage)
