09b95e17db84ddcd4435853ea363bfcf1251c6ac
sunuk-salad <113859258+sunuk-salad@users.noreply.github.com>
2023-05-31 16:46:14 -0700

    Azure devops (#1004)
    * Netlify ci/cd
    
    * Netlify ci/cd
    
    * Update Netlify azure pipeline
    
    * Fix environment name
    
    * Cleanup
    
    * temp comment yarn check
    
    * Add back yarn check
    
    * remove npmAlwaysAuth
    
    * Adding back npmAlwaysAuth  remove from yarnrc.yml
    
    * Update param

---
+++ packages/web-app/.azure-pipelines/build.ps1
@@ -0,0 +1,61 @@
+#!/usr/bin/env pwsh
+#Requires -Version 7
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $True)]
+    [string]$SiteName,
+    [Parameter(Mandatory = $True)]
+    [string]$Context
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$projectRoot = Split-Path -Path $PSScriptRoot -Parent
+Push-Location -Path $projectRoot
+try {
+    . (Join-Path -Path $projectRoot -ChildPath '.azure-pipelines' -AdditionalChildPath 'utilities.ps1')
+
+    # Setting up environment variables
+    Show-LogSection -Content 'Setting up environment variables...'
+    if ($Context -eq 'production') {
+        Show-LogInfo -Content 'Setting public url and mixpanel token.'
+        $Env:PUBLIC_URL = '/app'
+        if ($SiteName -eq 'test') {
+            $Env:REACT_APP_MIXPANEL_TOKEN = '4b245bace4eed86ffdfa35efc3addf1d'
+            $Env:REACT_APP_API_URL = 'https://app-api-testing.salad.com'
+            $Env:REACT_APP_PAYPAL_URL = 'https://www.sandbox.paypal.com/connect/?flowEntry=static&client_id=AYjYnvjB968mKTIhMqUtLlNa8CJuF9rg_Q4m0Oym5gFvBkZEMPPoooXcG94OjSCjih7kI1_KM25EgfDs&response_type=code&scope=openid%20email%20https%3A%2F%2Furi.paypal.com%2Fservices%2Fpaypalattributes&redirect_uri=https%253A%252F%252Fapp-api-testing.salad.com%252Fapi%252Fv2%252Fpaypal-account-callback'
+            $Env:REACT_APP_PROHASHING_USERNAME = 'saladtest'
+            $Env:REACT_APP_SEARCH_ENGINE = 'salad-rewards-test'
+            $Env:REACT_APP_SEARCH_KEY = 'search-qced4ibef8m4s7xacm9hoqyk'
+            $Env:REACT_APP_STRAPI_UPLOAD_URL = 'https://cms-api-testing.salad.io'
+            $Env:REACT_APP_UNLEASH_API_KEY = 'zrujLzhnwVZkIOlS74oZZ0DK7ZXs3Ifo'
+            $Env:REACT_APP_UNLEASH_URL = 'https://features-testing.salad.com/proxy'
+        }
+        else {
+            $Env:REACT_APP_MIXPANEL_TOKEN = '68db9194f229525012624f3cf368921f'
+        }
+    }
+
+    # Build projects.
+    Show-LogSection -Content 'Building projects...'
+    Show-LogCommand -Content "yarn run build "
+    & yarn run build
+    Assert-LastExitCodeSuccess -LastExecutableName 'yarn'
+}
+catch {
+    if (($null -ne $_.ErrorDetails) -and ($null -ne $_.ErrorDetails.Message)) {
+        Show-LogError -Content $_.ErrorDetails.Message
+    }
+    elseif (($null -ne $_.Exception) -and ($null -ne $_.Exception.Message)) {
+        Show-LogError -Content $_.Exception.Message
+    }
+    else {
+        Show-LogError -Content $_
+    }
+
+    exit 1
+}
+finally {
+    Pop-Location
+}
