9e9f96090a33dc8e9df53e8f46de41d5102fd998
Viktor <v.mozharovsky@gmail.com>
2023-09-01 20:24:57 +0200

    novu for unauthenticated user - fixed (#1018)

--- packages/web-app/src/modules/home-views/components/NavigationBarWithNotifications.tsx
+++ packages/web-app/src/modules/home-views/components/NavigationBarWithNotifications.tsx
@@ -4,25 +4,21 @@ import type { FunctionComponent } from 'react'
 import { getConfiguredNovuBannerNotifications } from '../../notifications/utils'
 
 export const NavigationBarWithNotifications: FunctionComponent<NavigationBarProps> = (props) => {
-  const {
-    notifications: novuNotifications,
-    unseenCount,
-    markNotificationAsRead,
-    markAllNotificationsAsSeen,
-  } = useNotifications()
+  const novu = useNotifications()
+  const novuNotifications = novu?.notifications || []
 
   const unreadNovuNotifications = novuNotifications?.filter((notification) => !notification.read)
   const bannerNotifications = getConfiguredNovuBannerNotifications(unreadNovuNotifications, (notificationId: string) =>
-    markNotificationAsRead(notificationId),
+    novu?.markNotificationAsRead(notificationId),
   )
   const newsNotifications = bannerNotifications.filter((notification) => notification?.variant === 'news')
   const warningsNotifications = bannerNotifications.filter((notification) => notification?.variant === 'error')
-  const hasUnseenNotifications = unseenCount > 0
+  const hasUnseenNotifications = novu?.unseenCount > 0
   const handleOpenNotificationsDrawer = () => {
     props.notifications.onOpenNotificationsDrawer()
 
     if (hasUnseenNotifications) {
-      markAllNotificationsAsSeen()
+      novu?.markAllNotificationsAsSeen()
     }
   }
 
