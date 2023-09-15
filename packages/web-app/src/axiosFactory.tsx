40a93152740f80d8011e1218306132db7fc2158e
Viktor <v.mozharovsky@gmail.com>
2023-09-15 15:00:06 +0200

    [WebApp] Identity core auth cookies (check of SuperToken session in response interceptor) (#1033)
    * Auth Logging and switching between auth types - added
    
    * super token session check - added to response interceptor

--- packages/web-app/src/axiosFactory.tsx
+++ packages/web-app/src/axiosFactory.tsx
@@ -40,13 +40,19 @@ export const createClient = (): AxiosInstance => {
     (response) => {
       // Any status code that lie within the range of 2xx cause this function to trigger
       // Do something with response data
+      console.log('Response Interceptor: ', response)
       return response
     },
     (error) => {
       try {
-        const store = getStore()
         if (error.response.status === 401 && error.config.baseURL === config.apiBaseUrl) {
-          store.auth.setIsAuthenticated(false)
+          SuperTokens.doesSessionExist().then((result) => {
+            if (!result) {
+              console.log('Response Interceptor: SuperToken session - doesn`t exist')
+              const store = getStore()
+              store.auth.setIsAuthenticated(false)
+            }
+          })
         }
       } finally {
         throw onError(error)
