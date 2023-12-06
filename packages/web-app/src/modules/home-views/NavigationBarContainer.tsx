26842123a8db15e51a2cffdfb96daf330800d36b
Viktor Mozharovskyi <v.mozharovsky@gmail.com>
2023-12-06 12:58:03 +0100

    InstallReminder content aligned to the right

--- packages/web-app/src/modules/home-views/NavigationBarContainer.tsx
+++ packages/web-app/src/modules/home-views/NavigationBarContainer.tsx
@@ -4,7 +4,6 @@ import type { RootStore } from '../../Store'
 import { connect } from '../../connect'
 import { InstallReminder } from './components/InstallReminder'
 import { NavigationBarWithNotifications } from './components/NavigationBarWithNotifications'
-import { detectBrowser } from './utils'
 
 const mapStoreToProps = (store: RootStore): any => {
   const isAuthenticated = store.auth.isAuthenticated
@@ -20,7 +19,6 @@ const mapStoreToProps = (store: RootStore): any => {
   const startButton = store.startButtonUI.properties
 
   const wasWidgetLoggedInOnce = !!store.profile.widgetFirstLoginDate
-  const { isChromeAgent } = detectBrowser()
 
   const targetReward: TargetRewardInfo | null = store.rewards.selectedTargetReward
     ? {
@@ -65,9 +63,7 @@ const mapStoreToProps = (store: RootStore): any => {
         isLoading={store.bonuses.pendingBonuses?.has(bonus.id)}
       />
     ) : undefined,
-    headerBannerContent: !wasWidgetLoggedInOnce && (
-      <InstallReminder justifyContent={isChromeAgent ? 'flex-end' : 'center'} />
-    ),
+    headerBannerContent: !wasWidgetLoggedInOnce && <InstallReminder justifyContent={'flex-end'} />,
     onClickAvatar: goToAccount,
     onClickUsername: goToAccount,
     onClickViewAllBonuses: () => store.routing.push('/account/bonuses'),
