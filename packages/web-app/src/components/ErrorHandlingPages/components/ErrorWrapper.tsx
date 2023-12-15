09323d182a52a85ea1f3723c4df608aea598ae77
Maksym Abramian <maksym.abramian@salad.com>
2023-12-15 13:56:34 +0200

    fix styling in error wrapper

--- packages/web-app/src/components/ErrorHandlingPages/components/ErrorWrapper.tsx
+++ packages/web-app/src/components/ErrorHandlingPages/components/ErrorWrapper.tsx
@@ -1,52 +1,47 @@
+import { Text } from '@saladtechnologies/garden-components'
+import type CSS from 'csstype'
 import type { FunctionComponent, ReactElement } from 'react'
 import type { WithStyles } from 'react-jss'
 import withStyles from 'react-jss'
 import type { SaladTheme } from '../../../SaladTheme'
 import ErrorBoundaryImage from '../assets/ErrorBoundaryImage.png'
 
-const styles = (theme: SaladTheme) => ({
+const styles: (theme: SaladTheme) => Record<string, CSS.Properties> = (theme: SaladTheme) => ({
   page: {
-    alignItems: 'center',
-    background: 'linear-gradient(124deg, #0A2133 55.6%, #3CC 145.45%)',
-    bottom: 0,
-    display: 'flex',
-    justifyContent: 'center',
-    left: 0,
     position: 'fixed',
+    bottom: 0,
+    left: 0,
     right: 0,
     top: 0,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'linear-gradient(124deg, #0A2133 55.6%, #3CC 145.45%)',
   },
   pageContent: {
     display: 'flex',
-    width: '110vmin',
-    height: '50vmin',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   errorBoundaryImage: {
-    maxWidth: 560,
-    height: 'auto',
-    width: '100%',
+    maxWidth: '56%',
   },
   textWrapper: {
     position: 'relative',
-    right: '16%',
-    top: '38%',
-    maxHeight: '150px',
+    right: '18%',
     maxWidth: '533px',
+    display: 'flex',
+    flexDirection: 'column',
+    width: '50%',
   },
   title: {
-    fontFamily: 'Mallory',
-    fontSize: '32px',
-    lineHeight: '34px',
     color: theme.green,
-    margin: 0,
-    marginBottom: '24px',
   },
   description: {
-    fontFamily: 'Mallory',
-    fontSize: '16px',
-    lineHeight: '24px',
     color: theme.white,
-    margin: 0,
+    marginTop: '24px',
   },
 })
 
@@ -60,8 +55,12 @@ const _ErrorWrapper: FunctionComponent<ErrorWrapperProps> = ({ title, descriptio
     <div className={classes.pageContent}>
       <img className={classes.errorBoundaryImage} src={ErrorBoundaryImage} alt="Salad Error Boundary" />
       <div className={classes.textWrapper}>
-        <p className={classes.title}>{title}</p>
-        <p className={classes.description}>{description}</p>
+        <Text className={classes.title} variant="baseXXL">
+          {title}
+        </Text>
+        <Text className={classes.description} variant="baseM">
+          {description}
+        </Text>
       </div>
     </div>
   </div>
