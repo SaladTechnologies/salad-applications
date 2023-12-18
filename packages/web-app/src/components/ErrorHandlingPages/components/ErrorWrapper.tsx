bec2040ecb39356654aad8b20c24bb62fd3aec45
Maksym Abramian <maksym.abramian@salad.com>
2023-12-18 14:49:12 +0200

    remove unused styles

--- packages/web-app/src/components/ErrorHandlingPages/components/ErrorWrapper.tsx
+++ packages/web-app/src/components/ErrorHandlingPages/components/ErrorWrapper.tsx
@@ -13,8 +13,6 @@ const styles: (theme: SaladTheme) => Record<string, CSS.Properties> = (theme: Sa
     left: 0,
     right: 0,
     top: 0,
-    width: '100%',
-    height: '100%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
