cfe0e58142593ac75e47042079a050dd9d3ba611
Ruslan <115642795+rners01@users.noreply.github.com>
2024-01-22 10:45:32 +0200

    [WebApp] Fix search sort value revert issue (#1118)
    * fix search sort value revert issue
    
    * fix comments

--- packages/web-app/src/components/Dropdown.tsx
+++ packages/web-app/src/components/Dropdown.tsx
@@ -1,8 +1,6 @@
-import type { ReactNode } from 'react'
-import { Component } from 'react'
+import type { FC } from 'react'
 import type { WithStyles } from 'react-jss'
 import withStyles from 'react-jss'
-import type { OptionsOrGroups } from 'react-select'
 import Select from 'react-select'
 import type { SaladTheme } from '../SaladTheme'
 
@@ -13,48 +11,57 @@ const styles = (theme: SaladTheme) => ({
     color: theme.lightGreen,
   },
 })
-
+interface Option {
+  label: string
+  value: string
+}
 interface Props extends WithStyles<typeof styles> {
-  options?: OptionsOrGroups<any, any>
+  options?: Option[]
+  value?: string
   onChange?: (value?: any) => void
 }
 
-const customStyles = {
-  singleValue: (provided: any, state: any) => {
-    const opacity = state.isDisabled ? 0.5 : 1
-    const transition = 'opacity 300ms'
-    const color = '#DBF1C1'
+const _Dropdown: FC<Props> = ({ classes, options, value, onChange }) => {
+  const selectedValue = value && options?.find((option) => option.value === value)
 
-    return { ...provided, opacity, transition, color }
-  },
-}
+  const customStyles = {
+    option: (styles: any) => {
+      return {
+        ...styles,
+        cursor: 'pointer',
+      }
+    },
+    singleValue: (provided: any, state: any) => {
+      const opacity = state.isDisabled ? 0.5 : 1
+      const transition = 'opacity 300ms'
+      const color = '#DBF1C1'
 
-class _Dropdown extends Component<Props> {
-  public override render(): ReactNode {
-    const { options, onChange, classes } = this.props
-
-    return (
-      <Select
-        className={classes.container}
-        options={options}
-        onChange={onChange}
-        defaultValue={options && options[0]}
-        styles={customStyles}
-        theme={(selectTheme) => ({
-          // TODO: Get the theme data using react-jss
-          ...selectTheme,
-          borderRadius: 0,
-          colors: {
-            ...selectTheme.colors,
-            neutral0: '#0A2133',
-            primary25: '#1F4F22',
-            primary: '#DBF1C1',
-            neutral20: '#DBF1C1',
-          },
-        })}
-      />
-    )
+      return { ...provided, opacity, transition, color }
+    },
   }
+
+  return (
+    <Select
+      className={classes.container}
+      value={selectedValue}
+      options={options}
+      onChange={onChange}
+      defaultValue={options && options[0]}
+      styles={customStyles}
+      theme={(selectTheme) => ({
+        // TODO: Get the theme data using react-jss
+        ...selectTheme,
+        borderRadius: 0,
+        colors: {
+          ...selectTheme.colors,
+          neutral0: '#0A2133',
+          primary25: '#1F4F22',
+          primary: '#DBF1C1',
+          neutral20: '#DBF1C1',
+        },
+      })}
+    />
+  )
 }
 
 export const Dropdown = withStyles(styles)(_Dropdown)
