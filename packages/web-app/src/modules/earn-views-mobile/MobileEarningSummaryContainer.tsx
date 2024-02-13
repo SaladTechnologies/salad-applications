eed4f19459dafab9ccf5eb0907627f2a439eae00
Maksym Abramian <maksym.abramian@salad.com>
2024-02-13 14:09:42 +0200

    add totals to mobile earn summary page

--- packages/web-app/src/modules/earn-views-mobile/MobileEarningSummaryContainer.tsx
+++ packages/web-app/src/modules/earn-views-mobile/MobileEarningSummaryContainer.tsx
@@ -7,9 +7,9 @@ const mapStoreToProps = (store: RootStore): any => ({
   currentBalance: store.balance.currentBalance,
   lifetimeBalance: store.balance.lifetimeBalance,
   totalXp: store.xp.currentXp,
-  last24Hr: store.balance.lastDayEarnings,
-  last7Day: store.balance.lastWeekEarnings,
-  last30Day: store.balance.lastMonthEarnings,
+  last24HrEarnings: store.balance.lastDayEarnings,
+  last7DayEarnings: store.balance.lastWeekEarnings,
+  last30DayEarnings: store.balance.lastMonthEarnings,
   earningHistory: store.balance.earningsHistory,
 })
 
