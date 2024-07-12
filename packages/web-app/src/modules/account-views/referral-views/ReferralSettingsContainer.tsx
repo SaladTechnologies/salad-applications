6d2c2f32af77f01beef385b1179f2180419b2cc1
Maks <41080668+Maks19@users.noreply.github.com>
2024-07-12 14:03:11 +0300

    update referral progress ui (#1158)

--- packages/web-app/src/modules/account-views/referral-views/ReferralSettingsContainer.tsx
+++ packages/web-app/src/modules/account-views/referral-views/ReferralSettingsContainer.tsx
@@ -2,6 +2,8 @@ import { connect } from '../../../connect'
 import type { RootStore } from '../../../Store'
 import { ReferralSettings } from './components/ReferralSettings'
 
-const mapStoreToProps = (_store: RootStore): any => ({})
+const mapStoreToProps = (store: RootStore): any => ({
+  referral: store.referral.currentReferral,
+})
 
 export const ReferralSettingsContainer = connect(mapStoreToProps, ReferralSettings)
--- packages/web-app/src/modules/account-views/referral-views/components/CurrentReferralPanel.tsx
+++
@@ -1,9 +0,0 @@
-import type { Referral } from '../../../referral/models'
-import { CurrentReferralProgress } from './CurrentReferralProgress'
-
-interface Props {
-  referral?: Referral
-}
-
-export const CurrentReferralPanel = ({ referral }: Props) =>
-  referral?.referralDefinition && <CurrentReferralProgress referral={referral} />
