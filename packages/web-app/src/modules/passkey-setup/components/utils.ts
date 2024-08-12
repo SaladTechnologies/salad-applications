29b6ed8e41798ad9d6c14a74d8f719d4f0640def
Viktor Mozharovskyi <v.mozharovsky@gmail.com>
2024-08-12 17:52:14 +0200

    getDefaultPasskeyName: refactored to switch

--- packages/web-app/src/modules/passkey-setup/components/utils.ts
+++ packages/web-app/src/modules/passkey-setup/components/utils.ts
@@ -1,44 +1,52 @@
 const getBrowserName = () => {
-  if (navigator.userAgent.indexOf('Opera') !== -1 || navigator.userAgent.indexOf('OPR') !== -1) {
-    return 'Opera'
+  switch (true) {
+    case navigator.userAgent.indexOf('Opera') !== -1 || navigator.userAgent.indexOf('OPR') !== -1:
+      return 'Opera'
+
+    case navigator.userAgent.indexOf('Edg') !== -1:
+      return 'Edge'
+
+    case navigator.userAgent.indexOf('Chrome') !== -1:
+      return 'Chrome'
+
+    case navigator.userAgent.indexOf('Safari') !== -1:
+      return 'Safari'
+
+    case navigator.userAgent.indexOf('Firefox') !== -1:
+      return 'Firefox'
+
     // @ts-ignore
-  } else if (navigator.userAgent.indexOf('Edg') !== -1) {
-    return 'Edge'
-  } else if (navigator.userAgent.indexOf('Chrome') !== -1) {
-    return 'Chrome'
-  } else if (navigator.userAgent.indexOf('Safari') !== -1) {
-    return 'Safari'
-  } else if (navigator.userAgent.indexOf('Firefox') !== -1) {
-    return 'Firefox'
-    // @ts-ignore
-  } else if (navigator.userAgent.indexOf('MSIE') !== -1 || !!document.documentMode === true) {
-    //IF IE > 10
-    return 'IE'
-  } else {
-    return 'Unknown Browser'
+    case navigator.userAgent.indexOf('MSIE') !== -1 || !!document.documentMode === true:
+      return 'IE'
+
+    default:
+      return 'Unknown Browser'
   }
 }
 
 const getOsName = () => {
   const userAgent = window.navigator.userAgent
   const platform = window.navigator.platform
-  let os = null
 
-  if (platform.indexOf('Win') !== -1) {
-    os = 'Windows'
-  } else if (platform.indexOf('Mac') !== -1) {
-    os = 'macOS'
-  } else if (platform.indexOf('Linux') !== -1) {
-    os = 'Linux'
-  } else if (/Android/.test(userAgent)) {
-    os = 'Android'
-  } else if (/iPhone|iPad|iPod/.test(userAgent)) {
-    os = 'iOS'
-  } else {
-    os = 'Unknown OS'
+  switch (true) {
+    case platform.indexOf('Win') !== -1:
+      return 'Windows'
+
+    case platform.indexOf('Mac') !== -1:
+      return 'macOS'
+
+    case platform.indexOf('Linux') !== -1:
+      return 'macOS'
+
+    case /Android/.test(userAgent):
+      return 'Android'
+
+    case /iPhone|iPad|iPod/.test(userAgent):
+      return 'iOS'
+
+    default:
+      return 'Unknown OS'
   }
-
-  return os
 }
 
 export const getDefaultPasskeyName = () => {
