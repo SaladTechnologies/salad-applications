5829d14a0dcbdbb189e09ad8b3a8e14651f6b007
Viktor Mozharovskyi <v.mozharovsky@gmail.com>
2024-08-21 00:33:09 +0200

    backup codes section - added

--- packages/web-app/src/modules/account-views/account-views/AccountSecurityContainer.tsx
+++ packages/web-app/src/modules/account-views/account-views/AccountSecurityContainer.tsx
@@ -6,7 +6,9 @@ const mapStoreToProps = (store: RootStore): any => ({
   fetchPasskeys: store.passkey.fetchPasskeys,
   onAddPasskeyClick: () => store.routing.push('/account/passkey/setup'),
   onDeletePasskeyClick: (passkeyId: string) => store.routing.push(`/account/passkey/delete/${passkeyId}`),
+  onViewBackupCodesClick: () => {},
   passkeys: store.passkey.passkeys,
+  withBackupCodes: true,
 })
 
 export const AccountSecurityContainer = connect(mapStoreToProps, AccountSecurity)
