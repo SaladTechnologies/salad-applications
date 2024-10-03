b1a4fee579b67332da3e23bbb55e1e84d3cf8d36
Viktor <v.mozharovsky@gmail.com>
2024-10-03 17:11:26 +0200

    [SALAD-22078] WebApp: EarningLineChart: filter inactive machines (#1210)
    * EarningLineChart: filter inactive machines
    
    * EarningLineChart: no data text changed

--- packages/web-app/src/modules/earn-views/EarningLineChartContainer.tsx
+++ packages/web-app/src/modules/earn-views/EarningLineChartContainer.tsx
@@ -5,6 +5,7 @@ import { EarningLineChart } from './components/EarningLineChart'
 const mapStoreToProps = (store: RootStore): any => {
   return {
     earningsPerMachine: store.balance.earningsPerMachine,
+    machines: store.balance.machines,
     daysShowing: store.balance.getDaysShowingEarnings,
     fetchEarningsPerMachine: store.balance.fetchEarningsPerMachine,
   }
