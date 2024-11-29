99849b960a09972e2c0c419bbca26ba44be7e1c3
Ruslan Ners <ruslan.nersezashvili@salad.com>
2024-11-29 08:17:50 +0800

    fix comment

--- packages/web-app/src/modules/demand-alerts-views/DemandAlertsPageContainer.tsx
+++ packages/web-app/src/modules/demand-alerts-views/DemandAlertsPageContainer.tsx
@@ -1,6 +1,6 @@
 import { connect } from '../../connect'
 import type { RootStore } from '../../Store'
-import { DemandAlertsPage } from './components'
+import { DemandAlertsPage } from './DemandAlertsPage'
 
 const mapStoreToProps = (_store: RootStore): any => ({})
 
