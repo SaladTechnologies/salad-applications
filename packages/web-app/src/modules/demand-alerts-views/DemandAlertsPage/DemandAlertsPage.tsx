9e14675899bd0c7ef8028157f94e4f63701aadde
Ruslan Ners <ruslan.nersezashvili@salad.com>
2024-12-02 14:35:59 +0800

    refactor layout

--- packages/web-app/src/modules/demand-alerts-views/DemandAlertsPage/DemandAlertsPage.tsx
+++ packages/web-app/src/modules/demand-alerts-views/DemandAlertsPage/DemandAlertsPage.tsx
@@ -1,8 +1,9 @@
-import { Text } from '@saladtechnologies/garden-components'
+import { Layout, Text } from '@saladtechnologies/garden-components'
 import type CSS from 'csstype'
 import Scrollbars from 'react-custom-scrollbars-2'
 import type { WithStyles } from 'react-jss'
 import withStyles from 'react-jss'
+import { Head } from '../../../components'
 import type { SaladTheme } from '../../../SaladTheme'
 import { withLogin } from '../../auth-views'
 
@@ -11,20 +12,7 @@ const styles: (theme: SaladTheme) => Record<string, CSS.Properties> = (theme: Sa
     backgroundImage: 'linear-gradient(to right, #53A626 , #B2D530)',
     color: theme.darkBlue,
     fontFamily: theme.fontMallory,
-    flex: 1,
     height: '100%',
-    padding: '25px 36px 25px',
-    '@media (min-width:600px)': {
-      padding: '50px 62px 14px',
-    },
-    '@media (min-width:1200px)': {
-      padding: '74px 104px 74px',
-    },
-  },
-  demandAlertsPageTitle: {
-    color: theme.lightGreen,
-    padding: 0,
-    marginBottom: '20px',
   },
   demandAlertsPageDescription: {
     display: 'block',
@@ -32,17 +20,19 @@ const styles: (theme: SaladTheme) => Record<string, CSS.Properties> = (theme: Sa
   },
 })
 
-interface Props extends WithStyles<typeof styles> {
-}
+interface Props extends WithStyles<typeof styles> {}
 
 const _DemandAlertsPage = ({ classes }: Props) => {
   return (
     <Scrollbars>
       <div className={classes.page}>
-        <Text className={classes.demandAlertsPageTitle} as="h1" variant="headline">
-          Demand Alerts
-        </Text>
-        <Text className={classes.demandAlertsPageDescription} variant="baseM">You can set up alerts to be notified when the demand level of a GPU reaches your sweet spot, even at a specific payout tier. When that scenario arrives we will notify you through email and an in-app message.</Text>
+        <Layout background="transparent" title="Demand Alerts">
+          <Head title="Demand Alerts" />
+          <Text className={classes.demandAlertsPageDescription} variant="baseM">
+            You can set up alerts to be notified when the demand level of a GPU reaches your sweet spot, even at a
+            specific payout tier. When that scenario arrives we will notify you through email and an in-app message.
+          </Text>
+        </Layout>
       </div>
     </Scrollbars>
   )
