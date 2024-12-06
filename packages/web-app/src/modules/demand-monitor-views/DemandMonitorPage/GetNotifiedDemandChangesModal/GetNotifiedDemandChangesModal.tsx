a201d81325b4d921f50b166fb35b1b4f1c7da5b8
Viktor Mozharovskyi <v.mozharovsky@gmail.com>
2024-12-06 16:44:21 +0100

    GetNotifiedDemandChangesModal: background image - improved

--- packages/web-app/src/modules/demand-monitor-views/DemandMonitorPage/GetNotifiedDemandChangesModal/GetNotifiedDemandChangesModal.tsx
+++ packages/web-app/src/modules/demand-monitor-views/DemandMonitorPage/GetNotifiedDemandChangesModal/GetNotifiedDemandChangesModal.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@saladtechnologies/garden-components'
 import type CSS from 'csstype'
-import { Img } from 'react-image'
 import type { WithStyles } from 'react-jss'
 import withStyles from 'react-jss'
 import type { SaladTheme } from '../../../../SaladTheme'
@@ -10,15 +9,23 @@ import type { DemandedHardwarePerformance } from '../../DemandMonitorStore'
 import saladBackgroundUrl from './assets/background.png'
 
 const styles: (theme: SaladTheme) => Record<string, CSS.Properties> = (theme: SaladTheme) => ({
+  modalWrapper: {
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    maxWidth: '700px',
+    maxHeight: '430px',
+  },
   modalContent: {
     position: 'relative',
-    width: '100%',
+    width: '80%',
     display: 'flex',
     backgroundColor: theme.darkBlue,
-    padding: '48px 48px 72px 180px',
+    padding: '48px 64px 72px 24px',
     flexDirection: 'column',
     alignItems: 'flex-start',
-    maxWidth: '800px',
     boxSizing: 'border-box',
   },
   title: {
@@ -50,10 +57,20 @@ const styles: (theme: SaladTheme) => Record<string, CSS.Properties> = (theme: Sa
     textDecoration: 'underline',
     cursor: 'pointer',
   },
+  saladImageWrapper: {
+    position: 'relative',
+    height: '500px',
+    width: '35%',
+  },
   saladImage: {
     position: 'absolute',
     top: '0px',
-    left: '0px',
+    right: '0px',
+    width: '100%',
+    height: '100%',
+    backgroundImage: `url(${saladBackgroundUrl})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain',
   },
   closeIcon: {
     position: 'absolute',
@@ -108,36 +125,40 @@ const _GetNotifiedDemandChangesModal = ({
 
   return (
     <ModalWithOverlay onCloseClick={onCloseClick}>
-      <div className={classes.modalContent}>
-        <h1 className={classes.title}>Get Notified Of Demand Changes</h1>
-        <p className={classes.description}>
-          You can set up alerts to be notified when the demand level of a GPU reaches your sweet spot, even at a
-          specific priority tier.
-        </p>
-        <div>
-          <h2 className={classes.subtitle}>Already have an account?</h2>
-          <p className={classes.description}>
-            Setting up alerts is easier and more powerful while{' '}
-            <span onClick={onLoginClick} className={classes.link}>
-              logged in
-            </span>
-          </p>
+      <div className={classes.modalWrapper}>
+        <div className={classes.saladImageWrapper}>
+          <div className={classes.saladImage} />
         </div>
-        <h2 className={classes.subtitle}>Not yet a chef? No problem!</h2>
-        <p className={classes.description}>
-          No need to create an account, simply select the GPU you wish to get alerts for and click continue.
-        </p>
-        <div className={classes.controlsWrapper}>
-          <div className={classes.dropdownWrapper}>
-            <div className={classes.dropdown}>
-              {demandHardwareOptions && <DropdownLight options={demandHardwareOptions} />}
+        <div className={classes.modalContent}>
+          <h1 className={classes.title}>Get Notified Of Demand Changes</h1>
+          <p className={classes.description}>
+            You can set up alerts to be notified when the demand level of a GPU reaches your sweet spot, even at a
+            specific priority tier.
+          </p>
+          <div>
+            <h2 className={classes.subtitle}>Already have an account?</h2>
+            <p className={classes.description}>
+              Setting up alerts is easier and more powerful while{' '}
+              <span onClick={onLoginClick} className={classes.link}>
+                logged in
+              </span>
+            </p>
+          </div>
+          <h2 className={classes.subtitle}>Not yet a chef? No problem!</h2>
+          <p className={classes.description}>
+            No need to create an account, simply select the GPU you wish to get alerts for and click continue.
+          </p>
+          <div className={classes.controlsWrapper}>
+            <div className={classes.dropdownWrapper}>
+              <div className={classes.dropdown}>
+                {demandHardwareOptions && <DropdownLight options={demandHardwareOptions} />}
+              </div>
+            </div>
+            <div className={classes.continueButtonWrapper}>
+              <Button variant="secondary" onClick={onContinuesClick} label="Continue" width={100} />
             </div>
           </div>
-          <div className={classes.continueButtonWrapper}>
-            <Button variant="secondary" onClick={onContinuesClick} label="Continue" width={100} />
-          </div>
         </div>
-        <Img className={classes.saladImage} src={saladBackgroundUrl} alt="salad-background" />
       </div>
     </ModalWithOverlay>
   )
