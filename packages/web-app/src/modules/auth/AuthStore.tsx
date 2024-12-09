1385498e39b64df6706ed7296279ea170aa0f9fb
Maksym Abramian <maksym.abramian@salad.com>
2024-12-09 13:44:38 +0200

    Redirect to Demand Alerts Page on successful login

--- packages/web-app/src/modules/auth/AuthStore.tsx
+++ packages/web-app/src/modules/auth/AuthStore.tsx
@@ -36,12 +36,12 @@ export class AuthStore {
   }
 
   @action
-  public login = async (): Promise<void> => {
+  public login = async (redirect_uri?: string): Promise<void> => {
     // Don't do anything if we are already logged in
     try {
       await this.axios.get('/api/v1/profile').then(() => runInAction(() => (this.isAuthenticated = true)))
     } catch {
-      const routeWithoutTrailingSlash = this.store.routing.location.pathname.slice(1)
+      const routeWithoutTrailingSlash = redirect_uri ?? this.store.routing.location.pathname.slice(1)
 
       window.location.assign(`${config.loginUrl}?redirect_uri=${routeWithoutTrailingSlash}`)
     }
