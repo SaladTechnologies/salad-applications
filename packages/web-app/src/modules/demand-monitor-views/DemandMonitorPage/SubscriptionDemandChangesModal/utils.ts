f4bd8b9c20fc932cf36ee55ec164379ce668d046
Viktor Mozharovskyi <v.mozharovsky@gmail.com>
2025-01-17 16:14:31 +0100

    load styles for mailchimp form ahead of time / second attempt mailchimp form usage leads to wrong in-demand device selected - fixed

--- packages/web-app/src/modules/demand-monitor-views/DemandMonitorPage/SubscriptionDemandChangesModal/utils.ts
+++ packages/web-app/src/modules/demand-monitor-views/DemandMonitorPage/SubscriptionDemandChangesModal/utils.ts
@@ -7,7 +7,6 @@ export interface GetMailchimpSubscriptionFormParams {
 
 export const getMailchimpSubscriptionForm = ({ id, fId, groupId, name }: GetMailchimpSubscriptionFormParams) => {
   return `<div id="mc_embed_shell">
-<link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 #mc_embed_signup{background:#fff; false;clear:left; font:14px Helvetica,Arial,sans-serif; width: 100%;}
 /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
