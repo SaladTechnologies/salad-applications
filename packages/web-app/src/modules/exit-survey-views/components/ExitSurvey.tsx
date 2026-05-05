29e32cf79f55a66dca72eb99de9403608f72ff00
Kyle Dodson <kyledodson@gmail.com>
2026-05-05 15:13:41 -0700

    Fix the exit survey URL

--- packages/web-app/src/modules/exit-survey-views/components/ExitSurvey.tsx
+++ packages/web-app/src/modules/exit-survey-views/components/ExitSurvey.tsx
@@ -1,20 +1,14 @@
 import type { FC } from 'react'
 import { useEffect } from 'react'
-import type { Profile } from '../../profile/models'
 
 interface Props {
-  currentProfile?: Profile
   trackUninstall: () => void
 }
 
-export const ExitSurvey: FC<Props> = ({ currentProfile, trackUninstall }) => {
+export const ExitSurvey: FC<Props> = ({ trackUninstall }) => {
   useEffect(() => {
     trackUninstall()
-    if (currentProfile?.id) {
-      window.location.replace(`https://p1atjy4hixj.typeform.com/saladexitsurvey#user_id=${currentProfile.id}`)
-    } else {
-      window.location.replace('https://p1atjy4hixj.typeform.com/saladexitsurvey')
-    }
-  }, [currentProfile?.id, trackUninstall])
+    window.location.replace('https://salad.com/exit-survey-landing')
+  }, [trackUninstall])
   return null
 }
